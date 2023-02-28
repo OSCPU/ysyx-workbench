e7e7a4eee7d993571dea1abbf9b5cbecec0aa5f6
Yuan <2252292157@qq.com>
2023-02-28 11:30:49 +0800

    NJU-ProjectN/am-kernels ics2021 initialized without tracing
    NJU-ProjectN/am-kernels 71196c72b57d5350db188645b9612d5357c8eaf6 thread-os: fix stack overflow with glibc 2.39

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -22,8 +22,8 @@ CXX := g++
 endif
 LD := $(CXX)
 INCLUDES = $(addprefix -I, $(INC_PATH))
-CFLAGS  := -O2 -MMD -Wall -Werror $(INCLUDES) $(CFLAGS)
-LDFLAGS := -O2 $(LDFLAGS)
+CFLAGS  := -O2 -MMD -Wall -Werror $(INCLUDES) $(CFLAGS) -fPIE
+LDFLAGS := -O2 $(LDFLAGS) -fPIE
 
 OBJS = $(SRCS:%.c=$(OBJ_DIR)/%.o) $(CXXSRC:%.cc=$(OBJ_DIR)/%.o)
 
