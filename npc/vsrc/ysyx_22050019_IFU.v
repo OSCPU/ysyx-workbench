b3fce6d60b5110f40b9c7ee966c1e083cefddc5f
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-08 21:44:36 +0800

    > sim RTL ysyx_22040000 张三 Linux zyx-desktop 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:44:36 up  2:51,  1 user,  load average: 0.37, 0.21, 0.24

--- npc/vsrc/ysyx_22050019_IFU.v
+++ npc/vsrc/ysyx_22050019_IFU.v
@@ -11,7 +11,7 @@ module ysyx_22050019_IFU#(
     input                 inst_j            ,
     input   [63:0]        snpc              ,  
     
-    output  [31:0]        inst_i            ,
+    input   [31:0]        inst_i            ,
     output  [63:0]        inst_addr_reg_o   , //送出去看指令的地址
 
     // 送出指令和对于pc的接口（打了一拍）
@@ -33,16 +33,14 @@ end
 
 assign inst_addr = inst_j?snpc:q;
 //=========================
-/* verilator lint_off UNUSED */reg [63:0] fetchmem_rdata;
-always @(*) begin
-  if (~rst_n) pmem_read(inst_addr, fetchmem_rdata);
-  else fetchmem_rdata = 64'b0;
-end
-assign inst_i = rst_n ? 32'b0 : fetchmem_rdata[31:0];
+
 
 //IFU第一级取指令流水操作
 ysyx_22050019_Reg #(32,32'b0) i0 (clk,rst_n,inst_i,inst_o,1'b1);
 ysyx_22050019_Reg #(64,64'b0) i1 (clk,rst_n,inst_addr,inst_addr_o,1'b1);
+
+//assign inst_addr_o = inst_addr;
+//assign inst_o      = inst_i;
 
 assign inst_addr_reg_o = 'b0;
 
--- npc/vsrc/ysyx_22050019_core.v
+++ npc/vsrc/ysyx_22050019_core.v
@@ -24,6 +24,13 @@ ysyx_22050019_IFU IFU
     .inst_o            (inst_if_id     )
 );
 
+//取出指令的逻辑分离出来
+/* verilator lint_off UNUSED */reg [63:0] fetchmem_rdata;
+always @(*) begin
+  if (~rst_n) pmem_read(inst_addr, fetchmem_rdata);
+  else fetchmem_rdata = 64'b0;
+end
+assign inst_i = rst_n ? 32'b0 : fetchmem_rdata[31:0];
 
 //decode模块端口
 //wire [63:0] inst_addr_id_ex;//decode流水
