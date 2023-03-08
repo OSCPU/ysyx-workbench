9186abde23485400195c1b1282347bf5a55d8ebd
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-08 22:30:12 +0800

    > sim RTL ysyx_22040000 张三 Linux zyx-desktop 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:30:12 up  3:37,  1 user,  load average: 0.42, 0.33, 0.27

--- npc/vsrc/ysyx_22050019_core.v
+++ npc/vsrc/ysyx_22050019_core.v
@@ -10,15 +10,14 @@ module ysyx_22050019_core(
 );
 
 //取出指令的逻辑分离出来
-/*
+
 wire [31:0]        inst_i ;
 fetch fetch_data(
     .clk (clk),
     .rst (rst_n),
-    .addr(inst_addr_if_id),
+    .addr(inst_addr),
     .data(inst_i)
 );
-*/
 
 //fetch模块端口
 ysyx_22050019_IFU IFU
@@ -29,9 +28,10 @@ ysyx_22050019_IFU IFU
     .inst_j            (inst_j),
     .snpc              (snpc|snpc_csr_id),
 
-    //.inst_i            (inst_i         ),
-    .inst_addr_o       (inst_addr_if_id), // 看指令执行进度的
+    .inst_i            (inst_i         ),
     .inst_addr_reg_o   (inst_addr),       //第二级流水指令
+
+    .inst_addr_o       (inst_addr_if_id), // 看指令执行进度的
     .inst_o            (inst_if_id     )
 );
 
