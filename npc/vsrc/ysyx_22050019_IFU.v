5598f18e1ff5da7aae7baaf8aec104fe89e8588a
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-10 21:23:19 +0800

    > sim RTL ysyx_22040000 张三 Linux zyx-desktop 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:23:19 up  2:29,  1 user,  load average: 0.51, 0.46, 0.41

--- npc/vsrc/ysyx_22050019_IFU.v
+++ npc/vsrc/ysyx_22050019_IFU.v
@@ -12,8 +12,8 @@ module ysyx_22050019_IFU#(
     input   [63:0]        snpc              ,  
     
     input  [31:0]         inst_i            ,
-    //input                 m_axi_arready     ,
-    //output                m_axi_arvalid     ,
+    //input                 m_axi_arvalid     ,
+    //output                m_axi_arready     ,
 
     output  [63:0]        inst_addr_reg_o   , //送出去看指令的地址
     //input                 m_axi_arready     ,
@@ -24,25 +24,19 @@ module ysyx_22050019_IFU#(
     output  [31:0]        inst_o
 );
 //=========================
+
+//=========================
 // pc 计数器
 reg [63:0]               inst_addr;
-reg [63:0]  q;
 always @(posedge clk) begin
     if (rst_n )
         inst_addr <= RESET_VAL ;
-    else
-        inst_addr <= inst_j ? snpc : inst_addr+64'd4    ;   
+    else if (inst_j)
+        inst_addr <= snpc ;   
 end
 
-//assign inst_addr = inst_j?snpc:q;
-//=========================
-
-//IFU第一级取指令流水操作
-ysyx_22050019_Reg #(32,32'b0) i0 (clk,rst_n,inst_i,inst_o,1'b1);
-ysyx_22050019_Reg #(64,64'b0) i1 (clk,rst_n,inst_addr,inst_addr_o,1'b1);
-
-//assign inst_addr_o = inst_addr;
-//assign inst_o      = inst_i;
+assign inst_addr_o = inst_addr;
+assign inst_o      = inst_i;
 
 assign inst_addr_reg_o = inst_addr;
 
