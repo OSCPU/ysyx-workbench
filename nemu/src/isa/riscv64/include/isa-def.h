34056486ab285122241a0b6daed02c111accb052
yzw <2086631242@qq.com>
2023-03-20 23:10:45 +0800

    NJU-ProjectN/am-kernels ics2021 initialized without tracing
    NJU-ProjectN/am-kernels 71196c72b57d5350db188645b9612d5357c8eaf6 thread-os: fix stack overflow with glibc 2.39

--- nemu/src/isa/riscv64/include/isa-def.h
+++ nemu/src/isa/riscv64/include/isa-def.h
@@ -25,7 +25,16 @@ typedef struct {
     struct {
       uint32_t opcode1_0 : 2;
       uint32_t opcode6_2 : 5;
-      uint32_t imm4_0    : 5;
+      uint32_t rd        : 5;
+      uint32_t funct3    : 3;
+      uint32_t rs1       : 5;
+      uint32_t rs2       : 5;
+      uint32_t funct7    : 7; //都是无符号型
+    } r;                      //例add R[rd]=R[rs1]+R[rs2]  funct7(7) rs2(5) rs1(5) funct3(3) rd(5) opcode(7)
+    struct {
+      uint32_t opcode1_0 : 2;
+      uint32_t opcode6_2 : 5;
+      uint32_t imm4_0    : 5;//simm11_5和imm4_0最后要combined在一起,所以simm11_5(高位)有符号,但是imm4_0无符号
       uint32_t funct3    : 3;
       uint32_t rs1       : 5;
       uint32_t rs2       : 5;
@@ -34,9 +43,29 @@ typedef struct {
     struct {
       uint32_t opcode1_0 : 2;
       uint32_t opcode6_2 : 5;
+      uint32_t imm11     : 1;
+      uint32_t imm4_1    : 4;
+      uint32_t funct3    : 3;
+      uint32_t rs1       : 5;
+      uint32_t rs2       : 5;
+      uint32_t simm10_5  : 6;
+      int32_t  sim12     : 1; //最高位   combined为imm[12:0] imm[0]alway0
+    } b;
+    struct {
+      uint32_t opcode1_0 : 2;
+      uint32_t opcode6_2 : 5;
       uint32_t rd        : 5;
       int32_t  simm31_12 :20;
     } u;
+    struct {
+      uint32_t opcode1_0 : 2;
+      uint32_t opcode6_2 : 5;
+      uint32_t rd        : 5;
+      uint32_t imm19_12  : 8;
+      uint32_t imm11     : 1;
+      uint32_t imm10_1   : 10;
+      int32_t  simm20    : 1;
+    } j;
     uint32_t val;
   } instr;
 } riscv64_ISADecodeInfo;
