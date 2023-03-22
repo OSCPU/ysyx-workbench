c8f3d955dec85c97baf5acf6d891cce82897331e
tracer-ics2021 <tracer@njuics.org>
2023-03-22 16:54:58 +0800

    >  compile 21009200811  Static hostname: DESKTOP-S81U2EJ        Icon name: computer-container          Chassis: container       Machine ID: 1149339005334af885f779b6c06deade          Boot ID: 5a1564d090c94e84b75dc3ad7932b9b8   Virtualization: wsl Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  16:54:58 up  1:32,  1 user,  load average: 0.03, 0.02, 0.00

--- nemu/src/isa/riscv64/instr/compute.h
+++ nemu/src/isa/riscv64/instr/compute.h
@@ -1,3 +1,133 @@
 def_EHelper(auipc) {
   rtl_li(s, ddest, id_src1->imm + s->pc);
 }
+def_EHelper(addi) {
+  rtl_addi(s, ddest, dsrc1, id_src2->imm);
+}
+
+def_EHelper(andi) {
+  rtl_andi(s, ddest, dsrc1, id_src2->imm);
+}
+
+def_EHelper(xori) {
+  rtl_xori(s, ddest, dsrc1, id_src2->imm);
+}
+
+def_EHelper(ori) {
+  rtl_ori(s, ddest, dsrc1, id_src2->imm);
+}
+
+
+def_EHelper(sll) {
+  rtl_sll(s, ddest, dsrc1, dsrc2);
+}
+
+def_EHelper(add) {
+  rtl_add(s, ddest, dsrc1, dsrc2);
+}
+
+def_EHelper(sub) {
+  rtl_sub(s, ddest, dsrc1, dsrc2);
+}
+
+def_EHelper(sltu) {
+  rtl_setrelop(s, RELOP_LTU, ddest, dsrc1, dsrc2);
+}
+
+def_EHelper(slt) {
+  rtl_setrelop(s, RELOP_LT, ddest, dsrc1, dsrc2);
+}
+
+def_EHelper(xor) {
+  rtl_xor(s, ddest, dsrc1, dsrc2);
+}
+
+def_EHelper(or) {
+  rtl_or(s, ddest, dsrc1, dsrc2);
+}
+
+def_EHelper(and) {
+  rtl_and(s, ddest, dsrc1, dsrc2);
+}
+
+def_EHelper(sra) {
+  rtl_sra(s, ddest, dsrc1, dsrc2);
+}
+
+def_EHelper(srai) {
+  rtl_srai(s, ddest, dsrc1, id_src2->imm & 0b111111u);
+}
+
+def_EHelper(srl) {
+  rtl_srl(s, ddest, dsrc1, dsrc2);
+}
+
+def_EHelper(srli) {
+  rtl_srli(s, ddest, dsrc1, id_src2->imm & 0b111111u);
+}
+
+def_EHelper(slli) {
+  rtl_slli(s, ddest, dsrc1, id_src2->imm & 0b111111u);
+}
+
+def_EHelper(beq) {
+  if (*dsrc1 == *ddest){
+    rtl_addi(s, &s->dnpc, &s->pc, id_src2->imm);
+  }
+}
+
+def_EHelper(bne) {
+  if (*dsrc1 != *ddest){
+    rtl_addi(s, &s->dnpc, &s->pc, id_src2->imm);
+  }
+}
+
+def_EHelper(bge) {
+  if ((sword_t)(*dsrc1) >= (sword_t)(*ddest)){
+    rtl_addi(s, &s->dnpc, &s->pc, id_src2->imm);
+  }
+}
+
+def_EHelper(bgeu) {
+  if ((*dsrc1) >= (*ddest)){
+    rtl_addi(s, &s->dnpc, &s->pc, id_src2->imm);
+  }
+}
+
+def_EHelper(blt) {
+  if ((sword_t)(*dsrc1) < (sword_t)(*ddest)){
+    rtl_addi(s, &s->dnpc, &s->pc, id_src2->imm);
+  }
+}
+
+def_EHelper(bltu) {
+  if ((*dsrc1) < (*ddest)){
+    rtl_addi(s, &s->dnpc, &s->pc, id_src2->imm);
+  }
+}
+
+def_EHelper(sltiu) {
+  rtl_setrelopi(s, RELOP_LTU, ddest, dsrc1, id_src2->imm);
+}
+
+def_EHelper(slti) {
+  rtl_setrelopi(s, RELOP_LT, ddest, dsrc1, id_src2->imm);
+}
+
+def_EHelper(jal) {
+  rtl_addi(s, ddest, &s->pc, 4);
+  rtl_addi(s, &s->dnpc, &s->pc, id_src1->imm);
+  stack_call(s->pc, s->dnpc);
+}
+
+def_EHelper(jalr) {
+  rtl_addi(s, s0, &s->pc, 4);
+  rtl_addi(s, &s->dnpc, dsrc1, id_src2->imm);
+  rtl_andi(s, &s->dnpc, &s->dnpc, ~1);
+  rtl_addi(s, ddest, s0, 0);
+  if (s->isa.instr.i.rd == 0 && s->isa.instr.i.rs1 == 1 && s->isa.instr.i.simm11_0 == 0){//Ret
+    stack_return(s->pc, s->dnpc);
+  }else{
+    stack_call(s->pc, s->dnpc);
+  }
+}
