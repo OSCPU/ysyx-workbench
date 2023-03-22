c07f1c38a624da26593fbf64f453d241d39b539c
tracer-ics2021 <tracer@njuics.org>
2023-03-22 16:34:06 +0800

    >  compile 21009200811  Static hostname: DESKTOP-S81U2EJ        Icon name: computer-container          Chassis: container       Machine ID: 1149339005334af885f779b6c06deade          Boot ID: 5a1564d090c94e84b75dc3ad7932b9b8   Virtualization: wsl Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  16:34:06 up  1:11,  1 user,  load average: 0.06, 0.01, 0.00

--- nemu/include/rtl/pseudo.h
+++ nemu/include/rtl/pseudo.h
@@ -17,12 +17,12 @@ static inline def_rtl(mv, rtlreg_t* dest, const rtlreg_t *src1) {
 
 static inline def_rtl(not, rtlreg_t *dest, const rtlreg_t* src1) {
   // dest <- ~src1
-  TODO();
+  rtl_xori(s, dest, src1, -1);
 }
 
 static inline def_rtl(neg, rtlreg_t *dest, const rtlreg_t* src1) {
   // dest <- -src1
-  TODO();
+  rtl_sub(s, dest, rz, src1);
 }
 
 static inline def_rtl(sext, rtlreg_t* dest, const rtlreg_t* src1, int width) {
