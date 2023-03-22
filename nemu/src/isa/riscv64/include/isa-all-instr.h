49f3bef3e089acbd409fbbe58569a74574ade1e3
tracer-ics2021 <tracer@njuics.org>
2023-03-22 17:07:31 +0800

    >  compile 21009200811  Static hostname: DESKTOP-S81U2EJ        Icon name: computer-container          Chassis: container       Machine ID: 1149339005334af885f779b6c06deade          Boot ID: 5a1564d090c94e84b75dc3ad7932b9b8   Virtualization: wsl Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  17:07:31 up  1:44,  1 user,  load average: 0.00, 0.01, 0.00

--- nemu/src/isa/riscv64/include/isa-all-instr.h
+++ nemu/src/isa/riscv64/include/isa-all-instr.h
@@ -1,13 +1,11 @@
 #include <cpu/decode.h>
 #include "../local-include/rtl.h"
-//f(ld) f(sd) 
-#define INSTR_LIST(f)  f(inv) f(nemu_trap) f(load) f(store) f(r_ty) f(i_ty) f(jal) f(jalr) f(branch) f(lui) f(ecall) f(ebreak) f(csrrw) f(csrrs)\
+//f(ld) f(sd) f(branch)
+#define INSTR_LIST(f)  f(inv) f(nemu_trap) f(load) f(store) f(r_ty) f(i_ty) f(jal) f(jalr)  f(lui) f(ecall) f(ebreak) f(csrrw) f(csrrs)\
 f(lb) f(lbu) f(lh) f(lhu) f(lw) f(sb) f(sh) f(sw)\
 f(addi) f(slli) f(slti) f(sltiu) f(xori) f(srli) f(srai) f(ori) f(andi)\
 f(add) f(sub) f(sll) f(slt) f(sltu) f(xor) f(srl) f(sra) f(or) f(and)\
 f(beq) f(bne) f(blt) f(bge) f(bltu) f(bgeu) f(auipc)
 
-// #define def_all_EXEC_ID() 
-//   INSTR_LIST(def_EXEC_ID)
 
 def_all_EXEC_ID();
