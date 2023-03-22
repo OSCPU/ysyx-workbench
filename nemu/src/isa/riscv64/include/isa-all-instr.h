414a7c39b100800ea6083f4be24355fad584a9b6
tracer-ics2021 <tracer@njuics.org>
2023-03-22 21:47:28 +0800

    >  compile 21009200811  Static hostname: DESKTOP-S81U2EJ        Icon name: computer-container          Chassis: container       Machine ID: 1149339005334af885f779b6c06deade          Boot ID: 5a1564d090c94e84b75dc3ad7932b9b8   Virtualization: wsl Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  21:47:28 up  6:24,  1 user,  load average: 0.14, 0.07, 0.01

--- nemu/src/isa/riscv64/include/isa-all-instr.h
+++ nemu/src/isa/riscv64/include/isa-all-instr.h
@@ -1,11 +1,11 @@
 #include <cpu/decode.h>
 #include "../local-include/rtl.h"
 //f(ld) f(sd) f(branch)
-#define INSTR_LIST(f)  f(inv) f(nemu_trap) f(csrrw) f(csrrs)\
-f(lb) f(lbu) f(lh) f(lhu) f(lw) f(sb) f(sh) f(sw)\
-f(addi) f(slli) f(slti) f(sltiu) f(xori) f(srli) f(srai) f(ori) f(andi)\
+#define INSTR_LIST(f)  f(inv) f(nemu_trap) f(addi) f(slli) f(slti) f(sltiu)\
+ f(xori) f(srli) f(srai) f(ori) f(andi)\
 f(add) f(sub) f(sll) f(slt) f(sltu) f(xor) f(srl) f(sra) f(or) f(and)\
 f(beq) f(bne) f(blt) f(bge) f(bltu) f(bgeu) f(auipc)
-
+//f(csrrw) f(csrrs)
+//f(lb) f(lbu) f(lh) f(lhu) f(lw) f(sb) f(sh) f(sw)
 
 def_all_EXEC_ID();
--- nemu/src/isa/riscv64/instr/compute.h
+++ nemu/src/isa/riscv64/instr/compute.h
@@ -1,11 +1,14 @@
-#include<rtl.h>
-#include<reg.h>
+
 
 def_EHelper(auipc) {
   rtl_li(s, ddest, id_src1->imm + s->pc);
 }
 def_EHelper(lui) {
   rtl_li(s, ddest, id_src1->imm);
+}
+
+def_EHelper(andi) {
+  rtl_andi(s, ddest, dsrc1, id_src2->imm);
 }
 
 def_EHelper(addi) {
