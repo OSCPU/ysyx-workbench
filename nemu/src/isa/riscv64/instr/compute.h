9c169a6816a7e3329d1eb02f6ae6ec5ef6a5fefc
tracer-ics2021 <tracer@njuics.org>
2023-03-22 16:56:01 +0800

    >  compile 21009200811  Static hostname: DESKTOP-S81U2EJ        Icon name: computer-container          Chassis: container       Machine ID: 1149339005334af885f779b6c06deade          Boot ID: 5a1564d090c94e84b75dc3ad7932b9b8   Virtualization: wsl Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  16:56:01 up  1:33,  1 user,  load average: 0.10, 0.05, 0.01

--- nemu/src/isa/riscv64/instr/compute.h
+++ nemu/src/isa/riscv64/instr/compute.h
@@ -1,6 +1,10 @@
 def_EHelper(auipc) {
   rtl_li(s, ddest, id_src1->imm + s->pc);
 }
+def_EHelper(lui) {
+  rtl_li(s, ddest, id_src1->imm);
+}
+
 def_EHelper(addi) {
   rtl_addi(s, ddest, dsrc1, id_src2->imm);
 }
