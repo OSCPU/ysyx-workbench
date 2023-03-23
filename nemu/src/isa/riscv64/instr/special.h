24bf66de25057d0acf614330c3e02419a34bc6b3
tracer-ics2021 <tracer@njuics.org>
2023-03-23 21:08:26 +0800

    >  compile 21009200811  Static hostname: DESKTOP-S81U2EJ        Icon name: computer-container          Chassis: container       Machine ID: 1149339005334af885f779b6c06deade          Boot ID: 5a1564d090c94e84b75dc3ad7932b9b8   Virtualization: wsl Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  21:08:26 up 1 day,  5:45,  1 user,  load average: 0.07, 0.08, 0.10

--- nemu/src/isa/riscv64/instr/special.h
+++ nemu/src/isa/riscv64/instr/special.h
@@ -6,8 +6,8 @@ def_EHelper(nemu_trap) {
   rtl_hostcall(s, HOSTCALL_EXIT, NULL, &gpr(10), NULL, 0); // gpr(10) is $a0
 }
 def_EHelper(ecall) {
-  rtl_hostcall(s, HOSTCALL_INV, NULL, NULL, NULL, 0);
+  rtl_hostcall(s, HOSTCALL_EXIT, NULL, &gpr(10), NULL, 0); // gpr(10) is $a0
 }
 def_EHelper(ebreak){
-  rtl_hostcall(s, HOSTCALL_INV, NULL, NULL, NULL, 0);
+  rtl_hostcall(s, HOSTCALL_EXIT, NULL, &gpr(10), NULL, 0); // gpr(10) is $a0
 }
