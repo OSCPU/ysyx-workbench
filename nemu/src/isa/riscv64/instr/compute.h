18f78ab4609057ef24e0e00b6641a9c8cc8fd8ad
tracer-ics2021 <tracer@njuics.org>
2023-03-23 17:01:37 +0800

    >  compile 21009200811  Static hostname: DESKTOP-S81U2EJ        Icon name: computer-container          Chassis: container       Machine ID: 1149339005334af885f779b6c06deade          Boot ID: 5a1564d090c94e84b75dc3ad7932b9b8   Virtualization: wsl Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  17:01:37 up 1 day,  1:39,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/isa/riscv64/instr/compute.h
+++ nemu/src/isa/riscv64/instr/compute.h
@@ -121,12 +121,24 @@ def_EHelper(slti) {
 
 def_EHelper(jal) {
   rtl_addi(s, ddest, &s->pc, 4);
-  rtl_addi(s, &s->dnpc, &s->pc, id_src1->imm<<1);
+  rtl_addi(s, &s->pc, &s->pc, id_src1->imm);
+  rtl_addi(s, &s->dnpc, &s->pc, 0);
 }
 
 def_EHelper(jalr) {
-  rtl_addi(s, s0, &s->pc, 4);
-  rtl_addi(s, &s->dnpc, dsrc1, id_src2->imm);
-  rtl_andi(s, &s->dnpc, &s->dnpc, ~1);
-  rtl_addi(s, ddest, s0, 0);
+  rtl_addi(s, ddest, &s->pc, 4);
+  rtl_addi(s, &s->pc, dsrc1, id_src2->imm);
+  rtl_addi(s, &s->dnpc, &s->pc, 0);
 }
+
+//   rtl_addi(s, s0, &s->pc, 4);
+//   rtl_addi(s, &s->dnpc, dsrc1, id_src2->imm);
+//   rtl_andi(s, &s->dnpc, &s->dnpc, ~1);
+//   rtl_addi(s, ddest, s0, 0);
+//   rtl_jr(s, &s->dnpc);
+
+
+
+
+
+
