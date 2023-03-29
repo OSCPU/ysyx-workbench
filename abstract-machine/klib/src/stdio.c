5aa34e554e6338467ccb567ef13676331e6e4a4a
tracer-ics2021 <tracer@njuics.org>
2023-03-29 14:16:24 +0800

    >  compile 21009200811  Static hostname: DESKTOP-S81U2EJ        Icon name: computer-container          Chassis: container       Machine ID: 1149339005334af885f779b6c06deade          Boot ID: b87b1556fbc645cfbab8b6f300e49356   Virtualization: wsl Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  14:16:24 up 17:17,  1 user,  load average: 0.02, 0.01, 0.00

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -44,14 +44,14 @@ int snprintf(char *out, size_t n, const char *format, ...) {
 
   return done;
 }
+
+
+
 //这个函数的基本思路是遍历格式化字符串，然后对于每个格式化指示符 `%`，根据其后面的字符来确定对应的参数类型,
 //并使用对应的 `va_arg` 宏从可变参数列表中取出相应的参数，最后将其格式化为字符串并复制到输出缓冲区中。
 //需要注意的是，为了能够正确处理各种格式化指示符，代码中涉及到了一些细节处理，例如：处理宽度、精度、十六进制输出、指针输出等。
 //同时，在将格式化结果复制到输出缓冲区时，需要检查是否会溢出输出缓冲区的大小，以避免造成安全问题。
 //最终，函数返回格式化输出的字符数，如果输出缓冲区的大小不足以容纳所有字符，函数会截断输出并返回 -1。
-
-
-
 int vsnprintf(char *str, size_t size, const char *format, va_list ap) {
   //panic("Not implemented");
     int written = 0;
@@ -164,62 +164,63 @@ int vsnprintf(char *str, size_t size, const char *format, va_list ap) {
                 return -1;
             }
             ++result;
-    } else if (type == 'x' || type == 'X') {
-        unsigned int value = va_arg(ap, unsigned int);
-        char buffer[32];
-        int len = sprintf(buffer, type == 'x' ? "%x" : "%X", value);
-        result += len;
-        if (written + len < size) {
-            int pad = width - len;
-            while (pad-- > 0) {
-                if (written + 1 < size) {
-                    str[written] = ' ';
-                    ++written;
-                } else {
-                    str[size - 1] = '\0';
-                    return -1;
-                }
-            }
-            memcpy(str + written, buffer, len);
-            written += len;
-        } else {
-            str[size - 1] = '\0';
-            return -1;
-        }
-    } else if (type == 'p') {
-        void *value = va_arg(ap, void *);
-        char buffer[32];
-        int len = sprintf(buffer, "%p", value);
-        result += len;
-        if (written + len < size) {
-            int pad = width - len;
-            while (pad-- > 0) {
-                if (written + 1 < size) {
-                    str[written] = ' ';
-                    ++written;
-                } else {
-                    str[size - 1] = '\0';
-                    return -1;
-                }
-            }
-            memcpy(str + written, buffer, len);
-            written += len;
-        } else {
-            str[size - 1] = '\0';
-            return -1;
-        }
-    } else {
-      // Unknown format specifier
-        if (written + 1 < size) {
-            str[written] = '%';
-            ++written;
-            str[written] = type;
-            ++written;
-        } else {
-            str[size - 1] = '\0';
-            return -1;
-        }
-    }
+     }
+    //  else if (type == 'x' || type == 'X') {
+    //     unsigned int value = va_arg(ap, unsigned int);
+    //     char buffer[32];
+    //     int len = sprintf(buffer, type == 'x' ? "%x" : "%X", value);
+    //     result += len;
+    //     if (written + len < size) {
+    //         int pad = width - len;
+    //         while (pad-- > 0) {
+    //             if (written + 1 < size) {
+    //                 str[written] = ' ';
+    //                 ++written;
+    //             } else {
+    //                 str[size - 1] = '\0';
+    //                 return -1;
+    //             }
+    //         }
+    //         memcpy(str + written, buffer, len);
+    //         written += len;
+    //     } else {
+    //         str[size - 1] = '\0';
+    //         return -1;
+    //     }
+    // } else if (type == 'p') {
+    //     void *value = va_arg(ap, void *);
+    //     char buffer[32];
+    //     int len = sprintf(buffer, "%p", value);
+    //     result += len;
+    //     if (written + len < size) {
+    //         int pad = width - len;
+    //         while (pad-- > 0) {
+    //             if (written + 1 < size) {
+    //                 str[written] = ' ';
+    //                 ++written;
+    //             } else {
+    //                 str[size - 1] = '\0';
+    //                 return -1;
+    //             }
+    //         }
+    //         memcpy(str + written, buffer, len);
+    //         written += len;
+    //     } else {
+    //         str[size - 1] = '\0';
+    //         return -1;
+    //     }
+    // } else {
+    //   // Unknown format specifier
+    //     if (written + 1 < size) {
+    //         str[written] = '%';
+    //         ++written;
+    //         str[written] = type;
+    //         ++written;
+    //     } else {
+    //         str[size - 1] = '\0';
+    //         return -1;
+    //     }
+    // }
   }
     if (written < size) {
       str[written] = '\0';
