d6be67a2e672eea7a8243cba85e49db8aabfd2d0
tracer-ics2021 <tracer@njuics.org>
2023-03-30 17:11:22 +0800

    >  compile 21009200811  Static hostname: DESKTOP-S81U2EJ        Icon name: computer-container          Chassis: container       Machine ID: 1149339005334af885f779b6c06deade          Boot ID: 39e4fe62e5a94bd895efbc67b82804ae   Virtualization: wsl Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  17:11:22 up 1 day, 4 min,  1 user,  load average: 0.10, 0.07, 0.01

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -9,6 +9,9 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
+  for (int i = 0; i < 32; i ++) {
+    printf("%s: 0x%08x\n", regs[i], reg_l(i));
+  }
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
