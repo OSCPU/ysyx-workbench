dae07f69226ddc982239fafb4c899730715a0ef5
tracer-ics2021 <tracer@njuics.org>
2023-03-30 17:23:49 +0800

    >  compile 21009200811  Static hostname: DESKTOP-S81U2EJ        Icon name: computer-container          Chassis: container       Machine ID: 1149339005334af885f779b6c06deade          Boot ID: 39e4fe62e5a94bd895efbc67b82804ae   Virtualization: wsl Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  17:23:49 up 1 day, 17 min,  1 user,  load average: 0.09, 0.09, 0.04

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -44,9 +44,10 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 
 static void print_iringbuf(int state){
   #ifdef CONFIG_ITRACE_COND
-  const char unsuccret[]="return unsuccesful at ";
-  puts(unsuccret);
+  
   if (state == NEMU_STOP||state==NEMU_ABORT){
+    const char unsuccret[]="return unsuccesful at ";
+    puts(unsuccret);
     for (int i = buf_cnt%BUF_DISPLAY_SIZE; i != BUF_DISPLAY_SIZE; i++,i=i%BUF_DISPLAY_SIZE){
         puts(iringbuf[i]);
     }
