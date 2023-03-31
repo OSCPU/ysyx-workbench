ae1657c202088777d58e9430dd6d9e7afcb2e5d1
tracer-ics2021 <tracer@njuics.org>
2023-03-31 15:45:37 +0800

    >  compile 21009200811  Static hostname: DESKTOP-S81U2EJ        Icon name: computer-container          Chassis: container       Machine ID: 1149339005334af885f779b6c06deade          Boot ID: 9b5312710ac240059be3877a749a735c   Virtualization: wsl Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  15:45:37 up  1:05,  1 user,  load average: 0.05, 0.02, 0.00

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -56,7 +56,7 @@ word_t paddr_read(paddr_t addr, int len) {
   }
   for(int i=(m_cnt+1)%M_TRACEL;i!=m_cnt%M_TRACEL;i++,(i)%=M_TRACEL){
     word_t w = pmem_read(m_tra[i], m_len[i]);
-    Log(" Read  from memory at %#.8x for %d bytes for %x.", m_tra[i], (int)m_len[i], (unsigned)w);
+    Log(" Read  from memory at %#.8x for %d bytes for %x.", m_tra[i], m_len[i], (unsigned)w);
   }
   MUXDEF(CONFIG_DEVICE, return mmio_read(addr, len),
     panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR ") at pc = " FMT_WORD,
