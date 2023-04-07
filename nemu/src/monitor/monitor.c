c31ad1d10b6c4bb7216cef316b18c684c0207747
tracer-ics2021 <tracer@njuics.org>
2023-04-07 11:45:27 +0800

    >  compile 21009200811  Static hostname: DESKTOP-S81U2EJ        Icon name: computer-container          Chassis: container       Machine ID: 1149339005334af885f779b6c06deade          Boot ID: c1ef2037e2a34ddfb871989d1ed86235   Virtualization: wsl Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  11:45:27 up 4 days, 21:22,  1 user,  load average: 0.15, 0.06, 0.06

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -85,6 +85,7 @@ static int parse_args(int argc, char *argv[]) {
         printf("\t-l,--log=FILE           output log to FILE\n");
         printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
         printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+        printf("\t-e,--elf=FILE           elf file to be parsed\n");
         printf("\n");
         exit(0);
     }
@@ -97,7 +98,6 @@ void init_monitor(int argc, char *argv[]) {
 
   /* Parse arguments. */
   parse_args(argc, argv);
-
   /* Set random seed. */
   init_rand();
 
