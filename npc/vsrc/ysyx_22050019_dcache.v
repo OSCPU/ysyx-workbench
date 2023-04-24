0e4a3b4e8ad6c5c17c0bd1058677c3d5acf0cb43
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-24 20:09:23 +0800

    > sim RTL ysyx_22050019 张宇鑫 Linux zyx-desktop 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:09:23 up 1 day,  4:57,  1 user,  load average: 0.41, 0.54, 0.71

--- npc/vsrc/ysyx_22050019_dcache.v
+++ npc/vsrc/ysyx_22050019_dcache.v
@@ -119,7 +119,7 @@ always@(*) begin
     RAM_CEN[0] = 1;
     RAM_CEN[1] = 1;
   end
-  else if((state == S_IDLE)&(next_state == S_HIT)&(ar_valid_i)|(state == S_R)&(next_state == S_HIT)|(next_state == S_AW)|(w_data_valid_i))
+  else if((state == S_IDLE)&(next_state == S_HIT)&(ar_valid_i)|(state == S_R)&(next_state == S_HIT)|(next_state == S_AW)|(w_data_valid_i&w_data_ready_o))
   RAM_CEN[hit_waynum_i|waynum] = 0;
   else
   RAM_CEN[hit_waynum_i|waynum] = 1;
