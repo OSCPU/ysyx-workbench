55eee02d7e1eb337adc01776c2fa97326faa55bc
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-25 16:28:09 +0800

    > sim RTL ysyx_22050019 张宇鑫 Linux zyx-desktop 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:28:09 up 2 days,  1:16,  1 user,  load average: 0.36, 0.45, 0.50

--- npc/csrc/memory/mem.c
+++ npc/csrc/memory/mem.c
@@ -37,8 +37,8 @@ extern "C" void pmem_read(ll raddr, ll *rdata)
    *rdata = ret;
    return ;
    }
-   printf("pmem_read-,addr = %016llx  rdata = %016llx is_skip_ref= %d\n",raddr,*rdata,is_skip_ref);
-   //if(raddr == )
+   //printf("pmem_read-,addr = %016llx  rdata = %016llx is_skip_ref= %d\n",raddr,*rdata,is_skip_ref);
+   //if(raddr == 0) return;
    // mimo设备访问
     *rdata=mmio_read((paddr_t)(raddr),8);
     return;
