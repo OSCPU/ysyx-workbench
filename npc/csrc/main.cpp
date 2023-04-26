34b740553997b69164dc4f3b16945c2fc0e5afbb
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-26 19:22:01 +0800

    > sim RTL ysyx_22050019 张宇鑫 Linux zyx-desktop 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:22:01 up 3 days,  4:09,  1 user,  load average: 0.89, 0.90, 0.92

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -117,7 +117,7 @@ void init_difftest() {
 
 void checkregs(uint64_t *ref_regs)
 {
-  IFDEF(DEBUG_DIFFTRACE, printf("diff_log: Difftest pc = 0x%016lx inst = 0x%016lx\n", dut->now_addr,dut->now_inst));
+  IFDEF(DEBUG_DIFFTRACE, printf("diff_log: Difftest pc = 0x%016lx inst = 0x%016x\n", dut->now_addr,dut->now_inst));
   for (int i = 0; i <= 36; ++i) {
     if (ref_regs[i] != cpu_gpr[i]) {
 
