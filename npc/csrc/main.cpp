82bc9b48f553313d99180cfe51834455117c81b1
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-01 13:37:48 +0800

    > sim RTL ysyx_22050019 张宇鑫 Linux zyx-desktop 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:37:48 up 3 days, 19:03,  1 user,  load average: 0.78, 0.35, 0.28

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -163,32 +163,20 @@ void difftest_exec_once()
   if (is_skip_ref) {
     //printf("is_skip_ref= %d\n",is_skip_ref);
     //防止递归失败的false设置，放在后面会被覆盖
-    is_skip_ref = false;
+    //is_skip_ref = false;
     //exec_once();
 //一个冒险的开关当开启这里时，会跳过连续访问外设的diff写reg覆盖，但会将访问后一条指令的结果直接写入参考模型，这是一个对正确性的隐患。（提升效果2~3倍）
-/*    while(is_skip_ref){
+    while(is_skip_ref){
     is_skip_ref = false;
+
+      while(difftest_ok){
+      exec_once();
+       }
 #ifdef CONFIG_ITRACE
   itrace_record(dut->now_addr);
 #endif
-    exec_once();
-    exec_once();
-    exec_once();
-      if(icache_exec){
-      icache_exec = false;
-      IFDEF(DEBUG_DIFFTRACE, printf("icache_exec \n"));
-      exec_once();
-      exec_once();
-      }
-      if(arbiter_exec){
-      arbiter_exec = false;
-      IFDEF(DEBUG_DIFFTRACE, printf("arbiter_exec \n"));
-      exec_once();
-      exec_once();
-      exec_once();
-      }
     }
- */   
+    
     ref_difftest_regcpy(cpu_gpr, DIFFTEST_TO_REF);
     //printf("time-last-is_skip_ref= %d\n",is_skip_ref);
     //printf("%lx\n",cpu_gpr[32]);
