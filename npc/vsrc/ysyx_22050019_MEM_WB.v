f8fa373d0dd8265922997b20caddc70f29c5f45b
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-11 22:05:59 +0800

    > sim RTL ysyx_22050019 张宇鑫 Linux zyx-desktop 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:05:59 up 3 days,  2:22,  1 user,  load average: 0.47, 0.40, 0.37

--- npc/vsrc/ysyx_22050019_MEM_WB.v
+++ npc/vsrc/ysyx_22050019_MEM_WB.v
@@ -22,33 +22,25 @@ module ysyx_22050019_MEM_WB (
 
   always @(posedge clk) begin
     if(rst_n) begin
+        pc_o   <= 0;
+        inst_o <= 0;
         reg_we_wbu_o         <= 0;
         reg_waddr_wbu_o      <= 0;
         reg_wdata_wbu_o      <= 0;
+        csr_regs_diff_o[0]   <= 0;
+        csr_regs_diff_o[1]   <= 0;
+        csr_regs_diff_o[2]   <= 0;
+        csr_regs_diff_o[3]   <= 0;
     end
     else begin
+        pc_o   <= pc_i  ;
+        inst_o <= inst_i;
         reg_we_wbu_o         <= reg_we_exu_lsu_i|reg_we_lsu_i  ;
         reg_waddr_wbu_o      <= reg_waddr_exu_i|reg_waddr_lsu_i;
         reg_wdata_wbu_o      <= reg_we_exu_lsu_i ? reg_wdata_exu_i|reg_wdata_csr_i : reg_we_lsu_i ? reg_wdata_lsu_i : 64'b0;
+        csr_regs_diff_o[3:0] <= csr_regs_diff_i[3:0];
     end
   end
 
-//======================================
-//仿真信号
-  always @(posedge clk) begin
-    if(rst_n) begin
-        pc_o             <= 0;
-        inst_o           <= 0;
-    for (int i = 0; i < 4; i++) begin
-      csr_regs_diff_o[i] <= 0;
-    end
-    end
-    else begin
-        pc_o            <= pc_i           ;
-        inst_o          <= inst_i         ;
-    for (int i = 0; i < 4; i++) begin
-      csr_regs_diff_o[i] <= csr_regs_diff_i[i];
-    end
-    end
-  end
+
 endmodule
