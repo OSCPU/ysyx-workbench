0c55f1b6ad2b908db2dc1cc0023b07dbdc50e754
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-12 16:11:08 +0800

    > sim RTL ysyx_22050019 张宇鑫 Linux zyx-desktop 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:11:08 up 3 days, 20:27,  1 user,  load average: 0.31, 0.14, 0.10

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -298,7 +298,8 @@ int main(int argc, char** argv, char** env) {
     cpu_reset();
     init_disasm("riscv64-pc-linux-gnu");
 //2流水线择在这里将cpu先跑一次，不可放在‘init_disasm（）’初始前
-    //exec_once();
+    exec_once();
+    exec_once();
     exec_once();
     exec_once();
     exec_once();
