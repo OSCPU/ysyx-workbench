5bd230d62fd2fcd490ce240085af4fcadca3c151
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-13 16:38:58 +0800

    > sim RTL ysyx_22050019 张宇鑫 Linux zyx-desktop 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:38:58 up 4 days, 20:55,  1 user,  load average: 0.36, 0.22, 0.12

--- npc/vsrc/ysyx_22050019_dcache.v
+++ npc/vsrc/ysyx_22050019_dcache.v
@@ -163,7 +163,7 @@ always@(*) begin
   if(rst)next_state=S_IDLE;
   else case(state)
     S_IDLE:if(ar_valid_i&ar_ready_o|aw_valid_i&aw_ready_o)begin
-            if(|hit_wayflag)next_state=S_HIT;
+            if(hit_wayflag[0]|hit_wayflag[1])next_state=S_HIT;
             if(dirty[random][index_in])next_state=S_AW;
             else next_state=S_AR;
           end
