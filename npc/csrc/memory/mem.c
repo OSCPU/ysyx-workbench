9491d5263cd7400383c19f202c6b9d4e72453fc5
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-13 15:13:02 +0800

    > sim RTL ysyx_22050019 张宇鑫 Linux zyx-desktop 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:13:02 up 4 days, 19:29,  1 user,  load average: 0.26, 0.26, 0.30

--- npc/csrc/memory/mem.c
+++ npc/csrc/memory/mem.c
@@ -38,8 +38,7 @@ extern "C" void pmem_read(ll raddr, ll *rdata)
    return ;
    }
    //printf("pmem_read-,addr = %016llx  rdata = %016llx is_skip_ref= %d\n",raddr,*rdata,is_skip_ref);
-   //printf("[Mimo - Read]: addr = %016llx data = %016llx\n", raddr, *rdata);
-   //if(raddr == 0x00000000a00003f8) debug_exit(1);
+   //if(raddr == 0) return;
    // mimo设备访问
     *rdata=mmio_read((paddr_t)(raddr),8);
     return;
@@ -60,7 +59,7 @@ extern "C" void pmem_write(ll waddr, ll wdata, char mask)
 
   // pemm地址内操作
   if (likely(in_pmem(waddr))){
-  //if(waddr == (ll)a00003f8) printf("[MTrace - Write]: addr = %016llx, data = %016llx  mask = 0x%02x\n", waddr, wdata, (unsigned char)mask);
+  //if(waddr == (ll)0x8000f004) printf("[MTrace - Write]: addr = %016llx, data = %016llx  mask = 0x%02x\n", waddr, wdata, (unsigned char)mask);
    uint8_t *pt = guest_to_host(waddr);
    for (int i = 0; i < 8; ++i) {
      if (mask & 1) *pt = (wdata & 0xff);
@@ -92,7 +91,6 @@ extern "C" void pmem_write(ll waddr, ll wdata, char mask)
     waddr = waddr + 1;
     wdata = wdata >> 8;
 } 
-  //printf("[Mimo - Write]: addr = %016llx, data = %016llx  mask = 0x%02x\n", waddr, wdata, (unsigned char)mask);
   // mimo设备访问
     mmio_write((paddr_t)waddr,wlen,(uint64_t)wdata); 
     return;
