02999cae39d53fdf9f14b58c0cc24c3b0b215336
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-14 08:41:47 +0800

    > sim RTL ysyx_22050019 张宇鑫 Linux zyx-desktop 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:41:47 up 5 days, 12:58,  1 user,  load average: 0.27, 0.14, 0.05

--- npc/vsrc/ysyx_22050019_dcache.v
+++ npc/vsrc/ysyx_22050019_dcache.v
@@ -312,7 +312,7 @@ assign cache_ar_valid_o = cache_ar_valid|next_state==S_AR;
 
 //仿真程序接入
 always@(posedge clk) begin
-  if(RAM_A == 6'h1b &&~write_enable&&RAM_WEN[0]) begin
+  if(RAM_A == 6'h1b &&~RAM_WEN[0]) begin
   $display("rwaddr   = %h\n\
      w_data_i       = %h\n\
      cache_r_data_i       = %h\n\
