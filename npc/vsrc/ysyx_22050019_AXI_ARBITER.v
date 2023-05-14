e5e45f141bd7b6775efd29581492a2d677ebeefe
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-14 17:49:52 +0800

    > sim RTL ysyx_22050019 张宇鑫 Linux zyx-desktop 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:49:52 up 5 days, 22:06,  1 user,  load average: 0.44, 0.26, 0.27

--- npc/vsrc/ysyx_22050019_AXI_ARBITER.v
+++ npc/vsrc/ysyx_22050019_AXI_ARBITER.v
@@ -110,16 +110,18 @@ assign s1_axi_r_valid_o=~r_channel?axi_r_valid_i:0;
 assign s1_axi_r_resp_o=~r_channel?axi_r_resp_i:0;
 assign s1_axi_r_data_o=~r_channel?axi_r_data_i:0;
 
-parameter RS_IDLE = 1'b0;
-parameter RS_S2   = 1'b1;//address handshake
+parameter RS_IDLE = 1;
+parameter RS_S1 = 2;
+parameter RS_S2 = 3;
+
 
 reg r_channel;
 wire w_channel = 1;
 
-reg rstate;
-reg next_rstate;
-
-//import "DPI-C" function void arbiter_wait();
+reg[2:0] rstate;
+reg[2:0] next_rstate;
+ 
+ //import "DPI-C" function void arbiter_wait();
     //// 读通道状态切换
 always@(posedge clk)begin
   if(rst)rstate<=RS_IDLE;
@@ -129,14 +131,16 @@ end
 always@(*) begin
   if(rst)next_rstate=RS_IDLE;
   else case(rstate)
-    RS_IDLE:if(s2_axi_ar_valid_i)next_rstate=RS_S2;
+    RS_IDLE:if(s1_axi_ar_valid_i)next_rstate=RS_S1;
+		  else if(s2_axi_ar_valid_i)next_rstate=RS_S2;
       else next_rstate=RS_IDLE;
-    RS_S2:if(axi_r_valid_i&s2_axi_r_ready_i)next_rstate=RS_IDLE;
+		RS_S1:if(s1_axi_r_ready_i&axi_r_valid_i)next_rstate=RS_IDLE;
+	    else next_rstate=RS_S1;
+		RS_S2:if(s2_axi_r_ready_i&axi_r_valid_i)next_rstate=RS_IDLE;
     else next_rstate=RS_S2;
     default:next_rstate=RS_IDLE;
   endcase
 end
-
 always@(posedge clk)begin
   if(rst)begin
         r_channel<=0;
@@ -151,6 +155,9 @@ always@(posedge clk)begin
       else begin
         r_channel<=0;
       end
+      RS_S1:if(next_rstate==RS_IDLE)begin
+        r_channel<=0;
+      end
       RS_S2:if(next_rstate==RS_IDLE)begin
         r_channel<=0;
       end
@@ -159,6 +166,4 @@ always@(posedge clk)begin
     endcase
   end
 end
-
-
 endmodule
