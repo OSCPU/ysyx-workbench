ed1b550b606fc0b40b19cc6a4f3f53f642ea5bbd
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-30 20:56:32 +0800

    > sim RTL ysyx_22050019 张宇鑫 Linux zyx-desktop 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:56:32 up  1:28,  1 user,  load average: 0.98, 0.72, 0.63

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -187,12 +187,12 @@ void difftest_exec_once()
     ref_difftest_regcpy(cpu_gpr, DIFFTEST_TO_REF);
     //printf("time-last-is_skip_ref= %d\n",is_skip_ref);
     //printf("%lx\n",cpu_gpr[32]);
-    printf("yes %lx\n",cpu_gpr[32]);
+    //printf("yes %lx\n",cpu_gpr[32]);
     return;
   }
   else{
   //printf("nemu-is_skip_ref= %d\n",is_skip_ref);
-  printf("no  %lx\n",cpu_gpr[32]);
+  //printf("no  %lx\n",cpu_gpr[32]);
   ref_difftest_exec(1);
   ref_difftest_regcpy(ref_regs, DIFFTEST_TO_DUT);
   //printf("is_skip_ref= %d\n",is_skip_ref);
