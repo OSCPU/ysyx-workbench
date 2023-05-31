6b9609a663d1e5de1c7dbf291557b5d3f05d44c3
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-31 10:56:44 +0800

    > compile NEMU ysyx_22050019 张宇鑫 Linux zyx-desktop 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:56:44 up 15:28,  1 user,  load average: 0.37, 0.68, 0.76

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -27,7 +27,7 @@ void init_proc() {
   Log("Initializing processes...");
 
   // load program here
-  naive_uload(NULL, "/bin/menu");
+  naive_uload(NULL, "/bin/pal");
 }
 
 Context* schedule(Context *prev) {
