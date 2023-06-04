afc333ba35440617f61b501656e5c171d72b43a3
ysyx_22050184_mac <qinbochen21@mails.ucas.ac.cn>
2023-06-05 00:16:26 +0800

    update

--- nemu/src/utils/filelist.mk
+++ nemu/src/utils/filelist.mk
@@ -15,6 +15,6 @@
 
 ifdef CONFIG_ITRACE
 CXXSRC = src/utils/disasm.cc
-CXXFLAGS += $(shell llvm-config --cxxflags) -fPIE
-LIBS += $(shell llvm-config --libs)
+CXXFLAGS += $(shell llvm-config-11 --cxxflags) -fPIE
+LIBS += $(shell llvm-config-11 --libs)
 endif
