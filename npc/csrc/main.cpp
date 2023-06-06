b5f9ab2e373ac1439bf029898fbaf8ac58b39198
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-06 14:10:41 +0800

    > sim RTL ysyx_22050019 张宇鑫 Linux zyx-desktop 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:10:41 up 19:59,  1 user,  load average: 0.48, 0.61, 0.71

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -309,13 +309,11 @@ int main(int argc, char** argv, char** env) {
     cpu_reset();
     init_disasm("riscv64-pc-linux-gnu");
 //2流水线择在这里将cpu先跑一次，不可放在‘init_disasm（）’初始前
-    exec_once();
-    exec_once();
-    exec_once();
-    exec_once();
-    exec_once();
-    exec_once();
-    exec_once();
+      while(difftest_ok == false){
+      exec_once();
+       }
+       difftest_ok = false;
+       
     //这一段的作用是将cpu的初始状态对齐到nemu中，所以在改进icache后，需要注意difftest肯也需要改进
 //    icache_exec = false;
     //difftest_ok = false;
