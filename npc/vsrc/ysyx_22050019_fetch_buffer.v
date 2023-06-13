ae9b5d77168ae8ce101814cac1ce96dafc24763c
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-13 21:36:09 +0800

    > sim RTL ysyx_22050019 张宇鑫 Linux zyx-desktop 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:36:09 up 7 days,  2:24,  1 user,  load average: 1.70, 1.66, 1.84

--- npc/vsrc/ysyx_22050019_fetch_buffer.v
+++ npc/vsrc/ysyx_22050019_fetch_buffer.v
@@ -1,7 +1,6 @@
 module ysyx_22050019_fetch_buffer#(
   parameter   WIDTH = 128,
-  parameter   RESET_VAL = 64'h80000000,
-  parameter   DEPTH = 4
+  parameter   RESET_VAL = 64'h80000000
 )(
   input                   clk         , 
   input                   rst_n       ,
@@ -178,7 +177,7 @@ wire               wfull ;
 wire               rempty;
 wire [WIDTH-1:0]   rdata ;
   // 用localparam定义一个参数，可以在文件内使用
-    localparam ADDR_WIDTH = DEPTH;
+    localparam ADDR_WIDTH = 1;
 
     reg [ADDR_WIDTH:0] waddr;
     reg [ADDR_WIDTH:0] raddr;
@@ -219,9 +218,9 @@ inst_buffer  buffer_regs
     (
     .clk  ( clk                   ),
     .wenc ( winc                  ),
-    .waddr( waddr[ADDR_WIDTH-2:0] ), 
+    .waddr( waddr[ADDR_WIDTH:0] ), 
     .wdata( wdata                 ),        
-    .raddr( raddr[ADDR_WIDTH-2:0] ), 
+    .raddr( raddr[ADDR_WIDTH:0] ), 
     .rdata( rdata                 )     
 );
 //=========================    
@@ -235,9 +234,9 @@ module inst_buffer #(
 (
   input                     clk  , 
   input                     wenc ,
-  input [DEPTH-2:0]         waddr,  //深度对2取对数，得到地址的位宽。
+  input [DEPTH-3:0]         waddr,  //深度对2取对数，得到地址的位宽。
   input [WIDTH-1:0]         wdata,  //数据写入
-  input [DEPTH-2:0]         raddr,  //深度对2取对数，得到地址的位宽。
+  input [DEPTH-3:0]         raddr,  //深度对2取对数，得到地址的位宽。
   output[WIDTH-1:0]         rdata   //数据输出
 );
 
