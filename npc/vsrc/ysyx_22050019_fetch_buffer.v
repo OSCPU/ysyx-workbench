3207eaf11cbd850419f92cef62d0a1cca9b633ae
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-15 14:46:29 +0800

    > sim RTL ysyx_22050019 张宇鑫 Linux zyx-desktop 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:46:29 up 1 day,  5:15,  1 user,  load average: 1.00, 0.52, 0.47

--- npc/vsrc/ysyx_22050019_fetch_buffer.v
+++ npc/vsrc/ysyx_22050019_fetch_buffer.v
@@ -56,10 +56,10 @@ always @ (posedge clk) begin
         rw_cnt <= rw_cnt;
     end
     else if(winc) begin
-        rw_cnt <= rw_cnt + 2'b1;
+        rw_cnt <= rw_cnt + 1'b1;
     end
     else if(rinc) begin
-        rw_cnt <= rw_cnt - 2'b1;
+        rw_cnt <= rw_cnt - 1'b1;
     end
     else begin
         rw_cnt <= rw_cnt;
@@ -71,7 +71,7 @@ end
 wire rinc = ~rempty && pc_changed;
 
 // 根据buffer状态和pc输出指令和指令有效使能
-assign inst_valid_o = pc_equal & ~rempty | ((pc_changed & ~jmp_flush_i) & rw_cnt != 2'b01)| rempty & r_valid_i & r_ready_o & ~jmp_flage;
+assign inst_valid_o = pc_equal & ~rempty | ((pc_changed & ~jmp_flush_i) & rw_cnt != 1'b1)| rempty & r_valid_i & r_ready_o & ~jmp_flage;
 
 assign inst_o       = inst_valid_o ? (pc_i[3] ? pc_i [2] ? rdata[127:96] : rdata[95:64] : pc_i [2] ? rdata[63:32] : rdata[31:0]) : 0;//仿真调试bug用，后期删除
 //=========================  
