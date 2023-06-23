3ee540d5e63ac0229a875ad950fb17df15a8d606
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-23 21:54:22 +0800

    > sim RTL ysyx_22050019 张宇鑫 Linux zyx-desktop 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:54:22 up 2 days,  8:13,  1 user,  load average: 1.64, 1.52, 1.31

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -27,7 +27,7 @@ void init_proc() {
   Log("Initializing processes...");
 
   // load program here
-  naive_uload(NULL, "/bin/pal");
+  naive_uload(NULL, "/bin/bird");
 }
 
 Context* schedule(Context *prev) {
