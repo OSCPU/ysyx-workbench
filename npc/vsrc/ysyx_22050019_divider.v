bcf3c111712fe5d55f621fefdd7447dd1deb4d54
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-23 19:52:30 +0800

    > sim RTL ysyx_22050019 张宇鑫 Linux zyx-desktop 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:52:30 up 2 days,  6:11,  1 user,  load average: 0.88, 0.88, 0.91

--- npc/vsrc/ysyx_22050019_divider.v
+++ npc/vsrc/ysyx_22050019_divider.v
@@ -204,7 +204,7 @@ always @(*) begin
         else begin
           next_state = DO_DIV;
           case (div_type_i)
-              DIV | REM: begin
+              DIV : begin
               cnt_next= 64;
               quotient_sign_next = dividend_i[63] ^ divisor_i[63];
               rem_sign_next = dividend_i[63];
@@ -212,7 +212,7 @@ always @(*) begin
               quotient_next[63:0] = dividend_abs;
               divisor_next = divisor_abs;
             end
-              DIVU | REMU: begin
+              DIVU: begin
                 cnt_next= 64;
                 quotient_sign_next = 0;
                 rem_sign_next = 0;
@@ -220,7 +220,7 @@ always @(*) begin
                 quotient_next[63:0] = dividend_i;
                 divisor_next = divisor_i;
               end
-              DIVUW | REMUW: begin
+              DIVUW: begin
                 cnt_next= 32;
                 quotient_sign_next = 0;
                 rem_sign_next = 0;
@@ -228,7 +228,7 @@ always @(*) begin
                 quotient_next[63:0] = {dividend_i[31:0], {32{1'b0}}};
                 divisor_next = {{32{1'b0}}, divisor_i[31:0]};
               end
-              DIVW | REMW: begin
+              DIVW: begin
                 cnt_next= 32;
                 quotient_sign_next = dividend_i[31] ^ divisor_i[31];
                 rem_sign_next = dividend_i[31];
