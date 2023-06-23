b81752117b5f136210560debac805aa038ab84f6
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-23 11:14:27 +0800

    > sim RTL ysyx_22050019 张宇鑫 Linux zyx-desktop 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:14:27 up 1 day, 21:33,  1 user,  load average: 0.22, 0.41, 0.49

--- npc/vsrc/ysyx_22050019_dcache.v
+++ npc/vsrc/ysyx_22050019_dcache.v
@@ -341,7 +341,7 @@ end
 generate
   genvar i;
   for(i=0;i<WAY_DEPTH;i=i+1)begin
-  assign hit_wayflag[i]=((tag[i][index_in]==tag_in)&&valid[i][index_in]);
+  assign hit_wayflag[i]=((tag[i][index_in]==tag_in) && valid[i][index_in]);
       S011HD1P_X32Y2D128_BW S011HD1P_X32Y2D128_BW_U0
       (
         .Q(RAM_Q[i]),
