4051e609ad3b10bf853c75322f190c268f4509a6
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-23 10:50:39 +0800

    > sim RTL ysyx_22050019 张宇鑫 Linux zyx-desktop 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:50:39 up 1 day, 21:09,  1 user,  load average: 1.15, 0.76, 0.57

--- npc/vsrc/ysyx_22050019_dcache.v
+++ npc/vsrc/ysyx_22050019_dcache.v
@@ -190,11 +190,11 @@ always@(posedge clk)begin
     addr                          <= 0                                     ;
     cache_rw_len_o                <= 0                                     ;
     //初始化对比项
-    for(integer i=0;i<WAY_DEPTH; i = i+1)begin
-      for(integer j=0;j<INDEX_DEPTH;j = j+1)begin
-          tag [i][j] <= 22'b0;
-	  			dirty[i][j]<= 1'b0;
-	  			valid[i][j]<= 1'b0;
+    for(integer m=0;m<WAY_DEPTH;m=m+1)begin
+      for(integer p=0;p<INDEX_DEPTH;p=p+1)begin
+          tag[m][p]<=0;
+	  			dirty[m][p]<=0;
+	  			valid[m][p]<=0;
       end
     end
   end
