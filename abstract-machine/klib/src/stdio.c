265e3e1104e7ac676d2de6f78a4f11e08d77c17b
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-30 19:49:00 +0800

    > compile NEMU ysyx_22050019 张宇鑫 Linux zyx-desktop 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:49:00 up  2:51,  1 user,  load average: 0.67, 0.71, 0.67

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,309 +5,430 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-char* itoa(int num, char* str, int radix)
-{
-  char index[]="0123456789ABCDEF";
-  unsigned int unum;
-  int i=0,j,k;
-  if(radix==-10&&num<0)
-  {
-    unum=(unsigned)(-num);
-    str[i++]='-';
-  }
-  else unum=(unsigned)num;
-	if(radix==-10)radix=10;
-  do
-  {
-    str[i++]=index[unum%(unsigned)radix];
-    unum/=radix;
-  }while(unum);
-  str[i]=0;
-  if(str[0]=='-')k=1;
-  else k=0;
-  char temp;
-  for(j=k;j<=(i-1)/2;j++)
-  {
-    temp=str[j];
-    str[j]=str[i-1+k-j];
-    str[i-1+k-j]=temp;
-  }
-  return str;
+int putchar(int c) {
+    putch(c);
+    return 1;
 }
 
-int printf(const char *fmt, ...) {
-  va_list ap;
-  int fmti=0;
-  int num=0;
-  va_start(ap,fmt);
-  while(*(fmt+fmti)!=0)
-  {
-    if(*(fmt+fmti)!='%')
-    {
-      num++;
-			putch(*(fmt+fmti));
+int countDigits(int num) {
+    // 计算有符号整数的位数
+    int count = 0;
+    if (num == 0) {
+        return 1;
+    } else if (num < 0) {
+        count++; // 负号占一位
+        num = -num;
     }
-    else
-    {
-      fmti++;
-      switch(*(fmt+fmti))
-      {
-        case '%':
-					putch('%');
-          num++;
-          break;
-        case 'c':
-          putch((char)va_arg(ap,int));
-          num++;
-          break;
-        case 'd':
-					{
-						int integer=va_arg(ap,int);
-						char str[32];
-						itoa(integer,str,-10);
-						int n=0;
-						while(*(str+n)!=0)
-						{
-							putch(*(str+n++));
-							num++;
-						}
-					}
-          break;
-        case 'u':
-					{
-						unsigned int integer=va_arg(ap,unsigned int);
-						char str[32];
-						itoa(integer,str,10);
-						int n=0;
-						while(*(str+n)!=0)
-						{
-							putch(*(str+n++));
-							num++;
-						}
-					}
-          break;
-        case 'x':
-        case 'p':
-					{
-						int integer=va_arg(ap,int);
-						char str[32];
-						itoa(integer,str,16);
-						int n=0;
-						while(*(str+n)!=0)
-						{
-							putch(*(str+n++));
-							num++;
-						}
-					}
-          break;
-        case 's':
-					{
-						char *str=va_arg(ap,char *);
-						int n=0;
-						while(*(str+n)!=0)
-						{
-							putch(*(str+n++));
-							num++;
-						}
-					}
-          break;
-        default:
-          return -1;
-          break;
-      }
+    while (num > 0) {
+        count++;
+        num /= 10;
     }
-		fmti++;
-  }
-  va_end(ap);
-  return num;
+    return count;
+}
+
+int intToCharArray(char* out, int num) {
+    int count = 0;  // 字符个数
+
+    if (num < 0) {
+        *out++ = '-';
+        num = -num;
+        count++;
+    }
+
+    if (num == 0) {
+        *out++ = '0';
+        count++;
+    } else {
+        char buffer[20];  // 保存转换后的字符
+        int idx = 0;      // 缓冲区索引
+
+        while (num > 0) {
+            buffer[idx++] = num % 10 + '0';
+            num /= 10;
+        }
+
+        while (idx > 0) {
+            *out++ = buffer[--idx];
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int countHexDigits(unsigned int num) {
+    // 计算无符号十六进制整数的位数
+    int count = 0;
+    if (num == 0) {
+        return 1;
+    }
+    while (num > 0) {
+        count++;
+        num /= 16;
+    }
+    return count;
+}
+
+int uintToCharArray(char* arr, unsigned int num) {
+    int count = 0;  // 字符个数
+
+    if (num == 0) {
+        arr[0] = '0';
+        arr[1] = '\0';
+        count = 1;
+    } else {
+        int index = 0;
+        while (num > 0) {
+            arr[index] = '0' + num % 10;
+            index++;
+            num /= 10;
+        }
+        arr[index] = '\0';
+        count = index;
+    }
+
+    return count;
+}
+
+
+int hexToCharArray(char* out, int num) {
+    int count = 0;  // 字符个数
+
+    if (num == 0) {
+        *out++ = '0';
+        count++;
+    } else {
+        char buffer[20];  // 保存转换后的字符
+        int idx = 0;      // 缓冲区索引
+
+        while (num > 0) {
+            int digit = num % 16;
+            buffer[idx++] = (digit < 10) ? (digit + '0') : (digit - 10 + 'a');
+            num /= 16;
+        }
+
+        while (idx > 0) {
+            *out++ = buffer[--idx];
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int printString(const char* str) {
+    // 打印字符串
+    // 返回打印的字符个数
+    int count = 0;
+    while (*str != '\0') {
+        putch(*str++);
+        count++;
+    }
+    return count;
+}
+
+int printDecimal(int num) {
+    // 打印有符号十进制整数
+    // 返回打印的字符个数
+    if (num == 0) {
+        putch('0');
+        return 1;
+    }
+
+    int count = 0;
+    if (num < 0) {
+        putch('-');
+        count++;
+        num = -num;
+    }
+
+    int digits[10];
+    int index = 0;
+
+    while (num > 0) {
+        digits[index] = num % 10;
+        num /= 10;
+        index++;
+    }
+
+    for (int i = index - 1; i >= 0; i--) {
+        putch('0' + digits[i]);
+        count++;
+    }
+
+    return count;
+}
+
+int printUnsignedDecimal(unsigned int num) {
+    // 打印无符号十进制整数
+    // 返回打印的字符个数
+    if (num == 0) {
+        putch('0');
+        return 1;
+    }
+
+    int count = 0;
+    unsigned int digits[10];
+    int index = 0;
+
+    while (num > 0) {
+        digits[index] = num % 10;
+        num /= 10;
+        index++;
+    }
+
+    for (int i = index - 1; i >= 0; i--) {
+        putch('0' + digits[i]);
+        count++;
+    }
+
+    return count;
+}
+
+int printHexadecimal(unsigned int num) {
+    // 打印有符号十六进制整数
+    // 返回打印的字符个数
+    if (num == 0) {
+        putch('0');
+        return 1;
+    }
+
+    int count = 0;
+    char digits[8];
+    int index = 0;
+
+    while (num > 0) {
+        int remainder = num % 16;
+        if (remainder < 10) {
+            digits[index] = '0' + remainder;
+        } else {
+            digits[index] = 'a' + remainder - 10;
+        }
+        num /= 16;
+        index++;
+    }
+
+    for (int i = index - 1; i >= 0; i--) {
+        putch(digits[i]);
+        count++;
+    }
+
+    return count;
+}
+
+int printf(const char* fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+
+    int num = 0;  // 输出字符的个数
+
+    while (*fmt != '\0') {
+        if (*fmt == '%') {
+            fmt++;  // 跳过 '%'
+            switch (*fmt) {
+                case 'c':
+                    putchar(va_arg(ap, int));
+                    num++;
+                    break;
+                case 'd':
+                    num += printDecimal(va_arg(ap, int));
+                    break;
+                case 'u':
+                    num += printUnsignedDecimal(va_arg(ap, unsigned int));
+                    break;
+                case 'x':
+                case 'p':
+                    num += printHexadecimal(va_arg(ap, int));
+                    break;
+                case 's':
+                    num += printString(va_arg(ap, const char*));
+                    break;
+                default:
+                    putchar('%');
+                    num++;
+                    break;
+            }
+        } else {
+            putchar(*fmt);
+            num++;
+        }
+
+        fmt++;
+    }
+
+    va_end(ap);
+    return num;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+    int outi = 0;
+    int num = 0;  // 输出字符的个数
+
+    while (*fmt != '\0') {
+        if (*fmt == '%') {
+            fmt++;  // 跳过 '%'
+            switch (*fmt) {
+                case 'c':
+                    out[outi++] = va_arg(ap, int);
+                    num++;
+                    break;
+                case 'd':
+                    {
+                        int integer = va_arg(ap, int);
+                        if (integer < 0) {
+                            out[outi++] = '-';
+                            num++;
+                            integer = -integer;
+                        }
+                        outi += intToCharArray(out + outi, integer);
+                        num += countDigits(integer) + 1;
+                    }
+                    break;
+                case 'u':
+                    {
+                        unsigned int integer = va_arg(ap, unsigned int);
+                        outi += uintToCharArray(out + outi, integer);
+                        num += countDigits(integer);
+                    }
+                    break;
+                case 'x':
+                case 'p':
+                    {
+                        int integer = va_arg(ap, int);
+                        outi += hexToCharArray(out + outi, integer);
+                        num += countHexDigits(integer);
+                    }
+                    break;
+                case 's':
+                    {
+                        const char* str = va_arg(ap, const char*);
+                        while (*str != '\0') {
+                            out[outi++] = *str++;
+                            num++;
+                        }
+                    }
+                    break;
+                default:
+                    out[outi++] = '%';
+                    num++;
+                    break;
+            }
+        } else {
+            out[outi++] = *fmt;
+            num++;
+        }
+
+        fmt++;
+    }
+
+    out[outi] = '\0';  // 添加字符串结束符
+    return num;
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-  va_list ap;
-  int outi=0;
-  int fmti=0;
-  int num=0;
-  va_start(ap,fmt);
-  while(*(fmt+fmti)!=0)
-  {
-			//printf("char:'%c'\n",*(fmt+fmti));
-    if(*(fmt+fmti)!='%')
-    {
-      *(out+outi++)=*(fmt+fmti);
-      num++;
-    }
-    else
-    {
-      fmti++;
-      switch(*(fmt+fmti))
-      {
-        case '%':
-          *(out+outi++)='%';
-          num++;
-          break;
-        case 'c':
-          *(out+outi++)=(char)va_arg(ap,int);
-          num++;
-          break;
-        case 'd':
-					{
-						int integer=va_arg(ap,int);
-						char str[32];
-						itoa(integer,str,-10);
-						int n=0;
-						while(*(str+n)!=0)
-						{
-							*(out+outi++)=*(str+n++);
-							num++;
-						}
-					}
-          break;
-        case 'u':
-					{
-						unsigned int integer=va_arg(ap,unsigned int);
-						char str[32];
-						itoa(integer,str,10);
-						int n=0;
-						while(*(str+n)!=0)
-						{
-							*(out+outi++)=*(str+n++);
-							num++;
-						}
-					}
-          break;
-        case 'x':
-        case 'p':
-					{
-						int integer=va_arg(ap,int);
-						char str[32];
-						itoa(integer,str,16);
-						int n=0;
-						while(*(str+n)!=0)
-						{
-							*(out+outi++)=*(str+n++);
-							num++;
-						}
-					}
-          break;
-        case 's':
-					{
-						char *str=va_arg(ap,char *);
-						int n=0;
-						while(*(str+n)!=0)
-						{
-							*(out+outi++)=*(str+n++);
-							num++;
-						}
-					}
-          break;
-        default:
-          return -1;
-          break;
-      }
-    }
-    fmti++;
-  }
-  va_end(ap);
-  *(out+outi)=0;
-  return num;
-}
 
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  va_list ap;
-  int outi=0;
-  int fmti=0;
-  int num=0;
-  va_start(ap,fmt);
-  while(*(fmt+fmti)!=0&&num<n)
-  {
-			//printf("char:'%c'\n",*(fmt+fmti));
-    if(*(fmt+fmti)!='%')
-    {
-      *(out+outi++)=*(fmt+fmti);
-      num++;
-    }
-    else
-    {
-      fmti++;
-      switch(*(fmt+fmti))
-      {
-        case '%':
-          *(out+outi++)='%';
-          num++;
-          break;
-        case 'c':
-          *(out+outi++)=(char)va_arg(ap,int);
-          num++;
-          break;
-        case 'd':
-					{
-						int integer=va_arg(ap,int);
-						char str[32];
-						itoa(integer,str,-10);
-						int n=0;
-						while(*(str+n)!=0)
-						{
-							*(out+outi++)=*(str+n++);
-							num++;
-						}
-					}
-          break;
-        case 'u':
-					{
-						unsigned int integer=va_arg(ap,unsigned int);
-						char str[32];
-						itoa(integer,str,10);
-						int n=0;
-						while(*(str+n)!=0)
-						{
-							*(out+outi++)=*(str+n++);
-							num++;
-						}
-					}
-          break;
-        case 'x':
-        case 'p':
-					{
-						int integer=va_arg(ap,int);
-						char str[32];
-						itoa(integer,str,16);
-						int n=0;
-						while(*(str+n)!=0)
-						{
-							*(out+outi++)=*(str+n++);
-							num++;
-						}
-					}
-          break;
-        case 's':
-					{
-						char *str=va_arg(ap,char *);
-						int n=0;
-						while(*(str+n)!=0)
-						{
-							*(out+outi++)=*(str+n++);
-							num++;
-						}
-					}
-          break;
-        default:
-          return -1;
-          break;
-      }
-    }
-    fmti++;
-  }
-  va_end(ap);
-  *(out+outi)=0;
-  return num;
+int sprintf(char* out, const char* fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    int result = vsprintf(out, fmt, ap);
+    va_end(ap);
+    return result;
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+    int outi = 0;
+    int num = 0; 
+    size_t remaining = n > 0 ? n - 1 : 0;  // 剩余可写入的字符数，预留一个字符位置用于添加字符串结束符
+
+    while (*fmt != '\0' && remaining > 0) {
+        if (*fmt == '%') {
+            fmt++;  // 跳过 '%'
+            switch (*fmt) {
+                case 'c':
+                    out[outi++] = va_arg(ap, int);
+                    num++;
+                    remaining--;
+                    break;
+                case 'd':
+                    {
+                        int integer = va_arg(ap, int);
+                        if (integer < 0) {
+                            out[outi++] = '-';
+                            num++;
+                            remaining--;
+                            integer = -integer;
+                        }
+                        int digits = countDigits(integer);
+                        if (digits <= remaining) {
+                            outi += intToCharArray(out + outi, integer);
+                            num += digits + 1;
+                            remaining -= digits;
+                        } else {
+                            break;
+                        }
+                    }
+                    break;
+                case 'u':
+                    {
+                        unsigned int integer = va_arg(ap, unsigned int);
+                        int digits = countDigits(integer);
+                        if (digits <= remaining) {
+                            outi += uintToCharArray(out + outi, integer);
+                            num += digits;
+                            remaining -= digits;
+                        } else {
+                            break;
+                        }
+                    }
+                    break;
+                case 'x':
+                case 'p':
+                    {
+                        int integer = va_arg(ap, int);
+                        int digits = countHexDigits(integer);
+                        if (digits <= remaining) {
+                            outi += hexToCharArray(out + outi, integer);
+                            num += digits;
+                            remaining -= digits;
+                        } else {
+                            break;
+                        }
+                    }
+                    break;
+                case 's':
+                    {
+                        const char* str = va_arg(ap, const char*);
+                        while (*str != '\0' && remaining > 0) {
+                            out[outi++] = *str++;
+                            num++;
+                            remaining--;
+                        }
+                    }
+                    break;
+                default:
+                    out[outi++] = '%';
+                    num++;
+                    remaining--;
+                    break;
+            }
+        } else {
+            out[outi++] = *fmt;
+            num++;
+            remaining--;
+        }
+
+        fmt++;
+    }
+
+    out[outi] = '\0';  // 添加字符串结束符
+    return num;
+}
+
+int snprintf(char* str, size_t size, const char* format, ...) {
+    va_list ap;
+    va_start(ap, format);
+    int ret = vsnprintf(str, size, format, ap);
+    va_end(ap);
+    return ret;
 }
 
 #endif
