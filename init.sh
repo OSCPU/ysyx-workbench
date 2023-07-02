e6c65e0d78860b62b43d831d33bef619f4871a5f
Zihao Yu <yuzihao@ict.ac.cn>
2023-07-02 16:46:01 +0800

    ready for 2306

--- init.sh
+++ init.sh
@@ -40,10 +40,10 @@ function init() {
 
 case $1 in
   nemu)
-    init NJU-ProjectN/nemu ysyx2204 nemu true NEMU_HOME
+    init NJU-ProjectN/nemu ics2023 nemu true NEMU_HOME
     ;;
   abstract-machine)
-    init NJU-ProjectN/abstract-machine ysyx2204 abstract-machine true AM_HOME
+    init NJU-ProjectN/abstract-machine ics2023 abstract-machine true AM_HOME
     init NJU-ProjectN/fceux-am ics2021 fceux-am false
     ;;
   am-kernels)
@@ -53,7 +53,7 @@ case $1 in
     init NJU-ProjectN/nanos-lite ics2021 nanos-lite true
     ;;
   navy-apps)
-    init NJU-ProjectN/navy-apps ics2021 navy-apps true NAVY_HOME
+    init NJU-ProjectN/navy-apps ics2023 navy-apps true NAVY_HOME
     ;;
   nvboard)
     init NJU-ProjectN/nvboard master nvboard false NVBOARD_HOME
@@ -63,7 +63,7 @@ case $1 in
       echo "chisel repo is already initialized, skipping..."
     else
       rm -rf npc
-      init OpenXiangShan/chisel-playground ysyx2204 npc true NPC_HOME
+      init OpenXiangShan/chisel-playground master npc true NPC_HOME
     fi
     ;;
   npc)
