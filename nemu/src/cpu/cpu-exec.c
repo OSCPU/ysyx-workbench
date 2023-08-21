4cf5e0d62abb4e6a1209a2fff0eff6a88af4789e
马梓恒 <1448364564@qq.com>
2023-08-21 18:52:20 +0800

    pa1 finish(watchpoint config)

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -42,9 +42,11 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
+#ifdef CONFIG_WATCHPOINT
   if(scan_head_list()){
     nemu_state.state = NEMU_STOP;
   }
+#endif 
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
