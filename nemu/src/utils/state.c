026f1e72aaac8fd877111d6d19e898cb2e6df96d
tracer-ysyx <tracer@ysyx.org>
2023-10-01 15:25:52 +0800

    > compile NEMU ysyx_22040000 张三 Linux corgi-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:25:52 up  5:29,  1 user,  load average: 0.12, 0.19, 0.18

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -15,11 +15,10 @@
 
 #include <utils.h>
 
-NEMUState nemu_state = { .state = NEMU_STOP };
+NEMUState nemu_state = { .state = NEMU_END };
 
 int is_exit_status_bad() {
   int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
-    (nemu_state.state == NEMU_STOP);
-    printf("%d %d %d",nemu_state.state,NEMU_STOP,NEMU_END);
+    (nemu_state.state == NEMU_QUIT);
   return !good;
 }
