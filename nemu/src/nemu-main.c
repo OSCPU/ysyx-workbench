3cf38ab4f042ebc7d6118a93432608fc5209d520
tracer-ysyx <tracer@ysyx.org>
2023-10-11 23:49:40 +0800

    > compile NEMU ysyx_22040000 张三 Linux corgi-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:49:40 up  6:36,  1 user,  load average: 0.15, 0.17, 0.12

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -36,11 +36,13 @@ word_t expr(char *e,bool *success);
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
+  /*
 #ifdef CONFIG_TARGET_AM
   am_init_monitor();
 #else
   init_monitor(argc, argv);
 #endif
+*/
 
   /* Start engine. */
   // engine_start();
