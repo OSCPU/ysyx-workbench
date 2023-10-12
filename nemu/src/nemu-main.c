fffec8a4fe2ddc7fa671d703b369bb0bb7460c79
tracer-ysyx <tracer@ysyx.org>
2023-10-12 09:09:00 +0800

    > compile NEMU ysyx_22040000 张三 Linux corgi-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:09:00 up 15:04,  1 user,  load average: 0.11, 0.14, 0.10

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -18,14 +18,13 @@
 #include <stdio.h>
 
 #include <stdlib.h>
-#include <string.h>
 #include <isa.h>
 #include <cpu/cpu.h>
 #include <memory/paddr.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 
-char *buf;
+char buf[65536]={};
 
 void init_monitor(int, char *[]);
 void am_init_monitor();
@@ -52,7 +51,7 @@ int main(int argc, char *argv[]) {
   unsigned int result;
   fp =fopen("./tools/gen-expr/input","r");
   assert(fp!=NULL);
-  int ret=fscanf(fp,"%u %[^\n]\n",&result,buf);
+  int ret=fscanf(fp,"%u %[^\n]%*c\n",&result,buf);
   if(ret==0) continue;
   fclose(fp);
   bool success=true;
