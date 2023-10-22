a1529ae04e82498daa5f0b9b0fbe86722823479e
tracer-ysyx <tracer@ysyx.org>
2023-10-22 15:51:04 +0800

    > compile NEMU ysyx_22040000 张三 Linux corgi-virtual-machine 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:51:04 up 34 min,  1 user,  load average: 0.22, 0.16, 0.16

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -82,8 +82,8 @@ static int cmd_info(char *args) {
 static int cmd_w(char *args) {
   bool success = true;
   WP *new = new_wp();
-  new->val = expr(args, &success);
   new->args = args;
+  new->val = expr(args, &success);
   if (!success) {
     printf("Bad expression,try again.\n");
     return 0;
