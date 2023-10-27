557d7ce8ccd6a4c3e2e25f88ca331da98b8e81d5
马梓恒 <1448364564@qq.com>
2023-10-27 18:11:48 +0800

    makefile

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -12,6 +12,7 @@ CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
 						 --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
+NPC_FLAGS += -e $(IMAGE).elf
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 .PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
 
@@ -19,3 +20,9 @@ image: $(IMAGE).elf
 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
+
+run: image
+	$(MAKE) -C $(NPC_HOME)/sim run ARGS="" IMG=$(IMAGE).bin
+
+gdb: image
+	$(MAKE) -C $(NPC_HOME)/sim gdb ARGS="" IMG=$(IMAGE).bin
