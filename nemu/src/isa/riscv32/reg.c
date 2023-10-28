ee1a101a494cf188d5d483382181de78413c0779
tracer-ysyx <tracer@ysyx.org>
2023-10-28 15:19:21 +0800

    > compile NEMU ysyx_22040000 张三 Linux corgi-virtual-machine 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:19:21 up  4:14,  1 user,  load average: 0.23, 0.17, 0.13

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -34,5 +34,17 @@ void isa_reg_display() {
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
+  if (!strcmp(s, "$0")) {
+    return cpu.gpr[0];
+  }
+  if (!strcmp(s, "$pc")) {
+    return cpu.pc;
+  }
+  for (int i = 1; i < 32; i++) {
+    if (!strcmp(s+1, regs[i])) {
+      return cpu.gpr[i];
+    }
+  }
+  *success = false;
   return 0;
 }
