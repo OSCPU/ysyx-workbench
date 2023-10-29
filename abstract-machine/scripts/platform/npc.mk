54cbae4801d01ca212e50cf17fffd66fa7e1a09a
马梓恒 <1448364564@qq.com>
2023-10-29 15:41:33 +0800

    Makefile modify

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -12,6 +12,7 @@ CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
 						 --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
+NPC_FLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt
 NPC_FLAGS += -e $(IMAGE).elf
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 .PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
@@ -22,7 +23,9 @@ image: $(IMAGE).elf
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
 run: image
-	$(MAKE) -C $(NPC_HOME)/sim run ARGS="" IMG=$(IMAGE).bin
+	$(MAKE) -C $(NPC_HOME) sim ARGS="$(NPC_FLAGS)" IMG=$(IMAGE).bin
 
+wave: image
+	gtkwave $(NPC_HOME)/sim/wave.vcd 
 gdb: image
-	$(MAKE) -C $(NPC_HOME)/sim gdb ARGS="" IMG=$(IMAGE).bin
+	$(MAKE) -C $(NPC_HOME) gdb ARGS="" IMG=$(IMAGE).bin
