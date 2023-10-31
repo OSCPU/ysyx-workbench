1c20aa489b1db58980bba06d3510d54100575777
tracer-ysyx <tracer@ysyx.org>
2023-10-31 07:29:13 -0700

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.4.0-152-generic #169~18.04.1-Ubuntu SMP Wed Jun 7 22:22:24 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  07:29:13 up  9:19,  1 user,  load average: 0.25, 0.17, 0.18

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -7,52 +7,34 @@
  
 #include "verilated_vcd_c.h" //可选，如果要导出vcd则需要加上
  
-// int main(int argc, char** argv, char** env) {
+int main(int argc, char** argv, char** env) {
  
-//   VerilatedContext* contextp = new VerilatedContext;
-//   contextp->commandArgs(argc, argv);
-//   Vtop* top = new Vtop{contextp};
+  VerilatedContext* contextp = new VerilatedContext;
+  contextp->commandArgs(argc, argv);
+  Vtop* top = new Vtop{contextp};
   
  
-//   VerilatedVcdC* tfp = new VerilatedVcdC; //初始化VCD对象指针
-//   contextp->traceEverOn(true); //打开追踪功能
-//   top->trace(tfp, 0); //
-//   tfp->open("wave.vcd"); //设置输出的文件wave.vcd
+  VerilatedVcdC* tfp = new VerilatedVcdC; //初始化VCD对象指针
+  contextp->traceEverOn(true); //打开追踪功能
+  top->trace(tfp, 0); //
+  tfp->open("wave.vcd"); //设置输出的文件wave.vcd
  
  
-//   while (!contextp->gotFinish()) {
-//     int a = rand() & 1;
-//     int b = rand() & 1;
-//     top->a = a;
-//     top->b = b;
-//     top->eval();
-//     printf("a = %d, b = %d, f = %d\n", a, b, top->c);
+  while (!contextp->gotFinish()) {
+    int a = rand() & 1;
+    int b = rand() & 1;
+    top->a = a;
+    top->b = b;
+    top->eval();
+    printf("a = %d, b = %d, f = %d\n", a, b, top->c);
  
-//     tfp->dump(contextp->time()); //dump wave
-//     contextp->timeInc(1); //推动仿真时间
+    tfp->dump(contextp->time()); //dump wave
+    contextp->timeInc(1); //推动仿真时间
  
-//     assert(top->c == a ^ b);
-//   }
-//   delete top;
-//   tfp->close();
-//   delete contextp;
-//   return 0;
-// }
-
-#include <nvboard.h>
-#include <Vtop.h>
-int main(){
-
-  VerilatedContext* contextp = new VerilatedContext;
-  Vtop* top = new Vtop{contextp};
-  nvboard_bind_all_pins(top);
-  nvboard_init();
-
-
-  while(1) {
-    nvboard_update();
-    top.eval();
-    // top.clk = 0; dut.eval();
-    // top.clk = 1; dut.eval();
+    assert(top->c == a ^ b);
   }
+  delete top;
+  tfp->close();
+  delete contextp;
+  return 0;
 }
