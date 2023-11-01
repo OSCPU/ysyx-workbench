41b5c5d1af01345528d1a45c261e27a5130bff74
tracer-ysyx <tracer@ysyx.org>
2023-11-01 02:11:36 -0700

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.4.0-152-generic #169~18.04.1-Ubuntu SMP Wed Jun 7 22:22:24 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:11:36 up  4:08,  1 user,  load average: 0.31, 0.22, 0.18

---
+++ npc/vsrc/top.v
@@ -0,0 +1,32 @@
+
+
+module top(
+    input [7:0]a ,
+    input [7:0]b ,
+    input [7:0]c ,
+    input [7:0]d ,
+    input [1:0]s ,
+    output reg [7:0]o
+);
+    
+
+    always @(*) begin
+
+        case(s)
+            0: o = a;
+            1: o = b;
+            2: o = c;
+            3: o = d;
+            default: o =0;
+        endcase
+    end
+
+    // assign ledup = sw[0] & sw[1];
+    // assign  ledr = { led , 7'b0000000 ,ledup};
+/*
+initial begin 
+    $display("Hello World"); 
+    //$finish; 
+end
+*/
+endmodule
