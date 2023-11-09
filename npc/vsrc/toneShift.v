cad25c8ca641a460044bdb97a29fdff0a6b02c4e
tracer-ysyx <tracer@ysyx.org>
2023-11-08 22:50:39 -0800

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.4.0-152-generic #169~18.04.1-Ubuntu SMP Wed Jun 7 22:22:24 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:50:39 up  5:54,  1 user,  load average: 0.78, 0.66, 0.44

--- npc/vsrc/toneShift.v
+++ npc/vsrc/toneShift.v
@@ -13,15 +13,15 @@ module RegShift#(int len)(
     always @(*)
         if(logicORalg == 1)begin
            if(left ==1) begin
-                out = {in[len - shiftStep:0] , shiftStep'b0};
+                out = {in[len - shiftStep:0] , {shiftStep{0}}};
            end 
            else begin
-                out = {shiftStep'b0 , in[len:len - shiftStep + 1]};
+                out = {{shiftStep{0}} , in[len:len - shiftStep + 1]};
            end
         end
         else begin
             if(left ==1)begin
-                out = {in[len] , in[len - shiftStep -1 :0] , shiftStep'b0};
+                out = {in[len] , in[len - shiftStep -1 :0] , {shiftStep{0}}};
             end
             else begin
                 out = {{(shiftStep+1){in[len]}} , in[len-1 : len-shiftStep]};
