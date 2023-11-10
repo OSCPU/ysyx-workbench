70fc6a7095d1f597bab6a8feef029c99b2d05a10
tracer-ysyx <tracer@ysyx.org>
2023-11-09 18:52:25 -0800

    > sim RTL ysyx_22040000 张三 Linux ubuntu 5.4.0-152-generic #169~18.04.1-Ubuntu SMP Wed Jun 7 22:22:24 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:52:25 up 1 day,  1:56,  1 user,  load average: 0.39, 0.27, 0.25

--- npc/vsrc/simpleStatus.v
+++ npc/vsrc/simpleStatus.v
@@ -9,11 +9,15 @@ module top(
     reg [31:0] cnt_zero , cnt_one;
 
     always @(posedge clk)
-        if(in == 1'b1)
-            cnt_one = cnt_one + 1;
+        if(in == 1'b1) begin
+            cnt_one <= cnt_one + 1;
+            cnt_zero <= 0 ;
+        end
         else
-            cnt_zero = cnt_zero +1;
-
+            begin
+            cnt_zero <= cnt_zero +1;
+            cnt_one <=0;
+            end
     assign c = (cnt_one >=4 || cnt_zero >=4) ? 1:0;
 
 endmodule
