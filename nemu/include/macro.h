825b1d1cc5668be8c0a83fcba724a60f467aa581
tracer-ysyx <tracer@ysyx.org>
2023-11-19 08:26:00 -0800

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 5.4.0-152-generic #169~18.04.1-Ubuntu SMP Wed Jun 7 22:22:24 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  08:25:59 up  7:34,  1 user,  load average: 0.54, 0.35, 0.26

--- nemu/include/macro.h
+++ nemu/include/macro.h
@@ -41,7 +41,7 @@
 #define MUX_WITH_COMMA(contain_comma, a, b) CHOOSE2nd(contain_comma a, b)
 #define MUX_MACRO_PROPERTY(p, macro, a, b) MUX_WITH_COMMA(concat(p, macro), a, b)
 // define placeholders for some property
-#define __P_DEF_0  X,
+#define __P_DEF_0  
 #define __P_DEF_1  X,
 #define __P_ONE_1  X,
 #define __P_ZERO_0 X,
