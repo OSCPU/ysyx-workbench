92c4a00c4fcee58fe4fc645e9b31740e01ac6ba5
yin1112 <yin1112@2857747701@qq.com>
2023-11-20 11:36:49 +0800

    fix merge conflict

--- nemu/include/macro.h
+++ nemu/include/macro.h
@@ -35,23 +35,13 @@
 #define concat4(x, y, z, w) concat3(concat(x, y), z, w)
 #define concat5(x, y, z, v, w) concat4(concat(x, y), z, v, w)
 
-<<<<<<< HEAD
-=======
-
->>>>>>> tracer-ysyx
 // macro testing
 // See https://stackoverflow.com/questions/26099745/test-if-preprocessor-symbol-is-defined-inside-macro
 #define CHOOSE2nd(a, b, ...) b
 #define MUX_WITH_COMMA(contain_comma, a, b) CHOOSE2nd(contain_comma a, b)
-<<<<<<< HEAD
-#define MUX_MACRO_PROPERTY(p, macro, a, b) MUX_WITH_COMMA(concat(p, macro), a, b)
-// define placeholders for some property
-#define __P_DEF_0  X,
-=======
 #define MUX_MACRO_PROPERTY(p, macro, a, b) MUX_WITH_COMMA( concat(p, macro) , a, b)
 // define placeholders for some property
 #define __P_DEF_0  
->>>>>>> tracer-ysyx
 #define __P_DEF_1  X,
 #define __P_ONE_1  X,
 #define __P_ZERO_0 X,
@@ -61,15 +51,7 @@
 #define MUXONE(macro, X, Y)  MUX_MACRO_PROPERTY(__P_ONE_, macro, X, Y)
 #define MUXZERO(macro, X, Y) MUX_MACRO_PROPERTY(__P_ZERO_,macro, X, Y)
 
-<<<<<<< HEAD
-// macro = 0 , X , Y
-// MUX_WITH_COMMA ( __P_DEF_0,  X , Y )
-// CHOOSE2nd(X, X,Y)
-=======
 
-// CHOOSE2nd( X X , Y)
-
->>>>>>> tracer-ysyx
 // test if a boolean macro is defined
 #define ISDEF(macro) MUXDEF(macro, 1, 0)
 // test if a boolean macro is undefined
