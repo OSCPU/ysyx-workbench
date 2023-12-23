9f9b4811c8a567389c33eaa7ca38c348d48e81b2
马梓恒 <1448364564@qq.com>
2023-12-23 20:16:36 +0800

    klib improved

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,73 +6,108 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int num2str(int num, char *str) {
-  int i = 0;
-  int isNegative = 0;
-  if (num < 0) {
-    num = -num;
-    isNegative = 1;
+#define SERIAL_BUF_SIZE 1000
+
+// Description: reverse the input string
+// @s         input string
+// @len       length of input string
+// Return     : none
+static void strrev(char *s, int len) {
+  char tmp,*start,*end;
+
+  start = s;
+  end   = s + len - 1;
+
+  while(start < end) {
+    tmp    = *start;
+    *start = *end;
+    *end   = tmp;
+
+    start++;
+    end--;
   }
-  do {
-    str[i++] = num % 10 + '0';
-    num /= 10;
-  } while (num > 0);
-  if (isNegative) {
-    str[i++] = '-';
-  }
-  str[i] = '\0';
-  int len = 0;
-  for(int i = 0; str[i] != '\0'; i++) {
-    len++;
-  }
-  for (int j = 0; j < len / 2; j++) {
-    char tmp = str[j];
-    str[j] = str[len - j - 1];
-    str[len - j - 1] = tmp;
-  }
-  return len;
 }
-static void reverse(char *s, int len) {
-  char *end = s + len - 1;
-  char tmp;
-  while (s < end) {
-    tmp = *s;
-    *s = *end;
-    *end = tmp;
-    s++;end--;
-  }
-}static int itoa(int n, char *s, int base) {
-  int i = 0, sign = n, bit;
-  if (sign < 0) n = -n;
+// Description: integer to string
+// @n          value of integer
+// @s          target string for converting
+// @radix      radix eg 10,16...
+// Retrun     : the length of string
+static int itoa(int n, char *s, int radix) {
+  int i, isNegative, bit;
+
+  isNegative = n < 0;
+  if(isNegative) n = -n;
+
+  i = 0;
   do {
-    bit = n % base;
+    bit = n % radix;
     if (bit >= 10) s[i++] = 'a' + bit - 10;
     else s[i++] = '0' + bit;
-  } while ((n /= base) > 0);
-  if (sign < 0) s[i++] = '-';
+  } while ((n /= radix) > 0);
+
+  if (isNegative) s[i++] = '-';
+
   s[i] = '\0';
-  reverse(s, i);
+
+  strrev(s, i);
 
   return i;
 }
 
+// Description: unsigned integer to string
+// @n          value of integer
+// @s          target string for converting
+// @radix      radix eg 10,16...
+static int uitoa(unsigned int n, char *s, int radix) {
+  int i, bit;
+
+  i = 0;
+  do {
+    bit = n % radix;
+    if (bit >= 10) s[i++] = 'a' + bit - 10;
+    else s[i++] = '0' + bit;
+  } while ((n /= radix) > 0);
+
+  s[i] = '\0';
+
+  strrev(s, i);
+
+  return i;
+}
 int vsprintf(char *out, const char *fmt, va_list ap) {
   char *start = out;
 
   for(; *fmt != '\0'; ++fmt) {
-    if(*fmt != '%') {
+    if(*fmt != '%') {            // normal character
       *out = *fmt;
       ++out;
-    } else {
-      switch (*(++fmt)) {
+    } else {                     // charachter with format
+      switch (*(++fmt)) {        // skip to next character
         case '%':
-          *out = *fmt; ++out; break;
+          *out = *fmt;
+          ++out;
+          break;
+        case 'b':
+          out += itoa(va_arg(ap, int), out, 2);
+          break;
+        case 'o':
+          out += itoa(va_arg(ap, int), out, 8);
+          break;
         case 'd':
-          out += itoa(va_arg(ap, int), out ,10); break;
+          out += itoa(va_arg(ap, int), out, 10);
+          break;
+        case 'u':
+          out += uitoa(va_arg(ap, int), out, 10);
+          break;
+        case 'x':case 'X':
+          out += itoa(va_arg(ap, int), out, 16);
+          break;
         case 's':
-          char *s = va_arg(ap, char *);
-          strcpy(out, s);
+          strcpy(out, va_arg(ap, char *));
           out += strlen(out);
+          break;
+        case 'p':
+          out += uitoa(va_arg(ap, int), out, 16);
           break;
       }
     }
@@ -83,52 +118,21 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  int cnt = 0;
-  int i;
-
   va_list args;
   va_start(args, fmt);
-  
-  for(i = 0; out[i] != '\0'; i++) {
-    out[i] = '\0';
-  }
 
-  for(i = 0; fmt[i] != '\0'; i++) {
-    cnt++;
-    if(fmt[i] == '%') {
-      i++;
-      switch (fmt[i]) {
-        case 'd':
-          int value = va_arg(args,int);
-          int sublen = num2str(value, out+cnt-1);
-          cnt += sublen - 1;
-          break;
-        case 's':
-          char *str = va_arg(args,char *);
-          for(int ii = 0; str[ii] != '\0'; ii++) {
-            out[cnt - 1] = str[ii];
-            cnt++;
-          }
-          cnt--;
-          break;
-      }
-    }
-    else {
-      out[cnt-1] = fmt[i];
-    }
-  }
+  int cnt = vsprintf(out, fmt, args);
 
   va_end(args);
 
   return cnt;
-  //panic("Not implemented");
 }
 
 int printf(const char *fmt, ...) {
   va_list args;
   va_start(args, fmt);
 
-  char serial_buf[10000];
+  char serial_buf[SERIAL_BUF_SIZE];
 
   int cnt = vsprintf(serial_buf, fmt, args);
   int i;
