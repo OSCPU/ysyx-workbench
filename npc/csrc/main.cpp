865dad87d3a315e15a2a1477e14a11e9ff831ac3
nomodeset <nomodeset@qq.com>
2024-03-07 11:41:48 +0000

    NJU-ProjectN/nvboard master initialized without tracing
    NJU-ProjectN/nvboard 7c360c777acbc2dd3f5d7ae8c50d96ca766b7996 Merge pull request #27 from wele0612/master

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "verilated.h"
+#include "verilated_vpi.h"
+#include "Vtop.h"
+#include "verilated_vcd_c.h"
 
-int main() {
+void sim_top(int sim_time) {
+  const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
+  const std::unique_ptr<Vtop> top{new Vtop{contextp.get()}};
+  Verilated::traceEverOn(true);
+  VerilatedVcdC* tfp = new VerilatedVcdC;
+  top->trace(tfp, 99);
+  tfp->open("obj_dir/simx.vcd");
+  while (contextp->time() < sim_time && !contextp->gotFinish()) {
+    contextp->timeInc(1);
+    top->a = rand() & 1;
+    top->b = rand() & 1;
+    top->eval();
+    printf("a = %d, b = %d, f = %d\n", top->a, top->b, top->f);
+    assert(top->f == (top->a ^ top->b));
+    tfp->dump(contextp->time());
+  }
+  tfp->close();
+}
+
+int main(int argc, char** argv) {
+  Verilated::commandArgs(argc, argv);
   printf("Hello, ysyx!\n");
+  sim_top(200);
   return 0;
 }
