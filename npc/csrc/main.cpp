9cc141100697a82f3c392d3f5ba2361f3a1fb8eb
tracer-ysyx <tracer@ysyx.org>
2024-05-24 19:14:05 +0800

    > sim RTL ysyx_22040000 smy Linux smy-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:14:05 up 17 min,  1 user,  load average: 0.42, 0.44, 0.34

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -6,26 +6,41 @@
 #include "verilated_vcd_c.h"
 #include "verilated.h"
 //static TOP_NAME dut;
-//static Vtop top;
+static Vtop* top;
 void nvboard_bind_all_pins(Vtop* top);
 
-int main(int argc, char** argv) {
+void single_cycle() {
+  top->clk = 0; top->eval();
+  top->clk = 1; top->eval();
+}
 
+void reset(int n) {
+  top->rst = 1;
+  while (n -- > 0) single_cycle();
+  top->rst = 0;
+}
+
+int main(int argc, char** argv) {
+/*
 	VerilatedContext* contextp = new VerilatedContext;
 	contextp->commandArgs(argc, argv);
 	Vtop* top = new Vtop{contextp};
 
 
-	//VerilatedVcdC* tfp = new VerilatedVcdC; //初始化VCD对象指针
-	//contextp->traceEverOn(true); //打开追踪功能
-	//top->trace(tfp, 0); //
-	//tfp->open("wave.vcd"); //设置输出的文件wave.vcd
 
 
+	VerilatedVcdC* tfp = new VerilatedVcdC; //初始化VCD对象指针
+	contextp->traceEverOn(true); //打开追踪功能
+	top->trace(tfp, 0); //
+	tfp->open("wave.vcd"); //设置输出的文件wave.vcd
+
+*/
 
 	nvboard_bind_all_pins(top);
 
 	nvboard_init();
+	reset(10);
+
 //!contextp->gotFinish()
 	while (1) {
 
@@ -40,6 +55,7 @@ int main(int argc, char** argv) {
 		//contextp->timeInc(1); //推动仿真时间
 		//assert(top->f == (a ^ b));
 		nvboard_update();
+		single_cycle();
 	}
 	//delete top;
 	//tfp->close();
