26eaab03adffeb96833e8e3d98887d7e996861be
tracer-ysyx <tracer@ysyx.org>
2024-05-29 15:59:44 +0800

    > sim RTL ysyx_22040000 smy Linux smy-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:59:44 up  2:57,  1 user,  load average: 0.18, 0.15, 0.27

--- npc/vsrc/addsub.v
+++ npc/vsrc/addsub.v
@@ -9,11 +9,13 @@ module addsub (
 );
     wire Cin;
     wire [31:0]B_eff;
+    wire [32:0]mid ;
     assign Cin = add_sub;
     assign B_eff = b^{32{add_sub}};
+    assign mid = a + B_eff+ {31'b0,Cin};
 
 
-    assign {carry,result} = a + B_eff + {31'b0,Cin};
+    assign {carry,result} = add_sub?{1'b0,mid[31:0]}:mid[32:0];
     assign zero= (result==0&&carry==0);
     assign overflow = (a[31]==B_eff[31]&&a[31]!=result[31]);
 
