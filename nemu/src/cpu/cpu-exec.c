9dc4bbd947f486fb628c61247703512a8d3612da
xXu Li Xuan <577783255@qq.com>
2025-01-17 19:45:35 +0800

    pa1 ultimate

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -44,10 +44,10 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 
 //check_watchpoints();
 
-#ifdef CONFIG_WATCHPOINT
+
   // 在每次执行时检查监视点
   check_watchpoints();
-#endif
+
 
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 }
@@ -151,10 +151,10 @@ void cpu_exec(uint64_t n) {
 
   uint64_t timer_start = get_time();
 
-//  execute(n);
+  execute(n);
 
 
-   for (uint64_t i = 0; i < n; i++) {
+ /*  for (uint64_t i = 0; i < n; i++) {
     execute(1); // 执行单个指令
     check_watchpoints(); // 每执行一步就检查监视点
 	if(nemu_state.state == NEMU_STOP)
@@ -164,8 +164,7 @@ void cpu_exec(uint64_t n) {
         
 
 	}
-  }
-
+  }*/
   uint64_t timer_end = get_time();
   g_timer += timer_end - timer_start;
 
