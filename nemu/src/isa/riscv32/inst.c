71cc963bf111fa56a947a79fcf8fb20108fef29b
xXu Li Xuan <577783255@qq.com>
2025-02-11 23:11:18 +0800

    dummy所有指令 jal ret addi li mv sw 全部通过hit good trap

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -23,7 +23,7 @@
 #define Mw vaddr_write
 
 enum {
-  TYPE_I, TYPE_U, TYPE_S,
+  TYPE_I, TYPE_U, TYPE_S,TYPE_J,
   TYPE_N, // none
 };
 
@@ -32,6 +32,13 @@ enum {
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
+#define immJ() do { \
+    *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | \
+           (BITS(i, 30, 21) << 1) | \
+           (BITS(i, 20, 20) << 11) | \
+           (BITS(i, 19, 12) << 12); \
+} while (0)
+
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst;
@@ -42,6 +49,8 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_I: src1R();          immI(); break;
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
+		case TYPE_J: immJ(); break;
+
     case TYPE_N: break;
     default: panic("unsupported type = %d", type);
   }
@@ -63,7 +72,16 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? 100 ????? 00000 11", lbu    , I, R(rd) = Mr(src1 + imm, 1));
   INSTPAT("??????? ????? ????? 000 ????? 01000 11", sb     , S, Mw(src1 + imm, 1, src2));
 
-INSTPAT("??????? ????? 000 ????? ????? 0010011", li   , I, R(rd) =imm);  // 将 li 改为 addi 指令
+//INSTPAT("??????? ????? 000 ????? ????? 0010011", addi   , I, R(rd) =src1+imm);  // 将 li 改为 addi 指令 li+mv is the special consequence
+
+INSTPAT("??????? ????? ????? ????? 1101111", jal, J,R(rd) = s->pc + 4; s->dnpc=s->pc+imm);
+
+INSTPAT("??????? ????? ????? 000 ????? 0010011", addi, I, R(rd) = src1 + imm);
+INSTPAT("??????? ????? ????? 000 ????? 1100111", ret, I, s->dnpc = R(1));
+
+
+INSTPAT("??????? ????? ????? 010 ????? 0100011", sw, S, Mw(src1 + imm, 4, src2));
+
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
