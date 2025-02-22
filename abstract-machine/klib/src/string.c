464fe7674399946ec29d93803540ae4692fc48a3
xXu Li Xuan <577783255@qq.com>
2025-02-22 23:22:18 +0800

    为什么nemu/下的makefile 修改批处理以后我的cpu-tests也会进行批处理怎么连接的，我把sring搞好了测试通过了

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,11 +5,20 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+ // panic("Not implemented");
+  size_t len = 0;
+    while (s[len] != '\0') {
+        len++;
+    }
+    return len;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  //panic("Not implemented");
+	char *ret = dst;
+    while ((*dst++ = *src++) != '\0')
+        ;
+    return ret;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
@@ -17,11 +26,23 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  //panic("Not implemented");
+	char *ret = dst;
+    while (*dst) {  // Move to the end of dest
+        dst++;
+    }
+    while ((*dst++ = *src++) != '\0')  // Append src to dest
+        ;
+    return ret;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+//  panic("Not implemented");
+	while (*s1 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+    }
+    return *(unsigned char *)s1 - *(unsigned char *)s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
@@ -29,7 +50,11 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+   unsigned char *ptr = s;
+    while (n--) {
+        *ptr++ = (unsigned char)c;
+    }
+    return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -37,11 +62,26 @@ void *memmove(void *dst, const void *src, size_t n) {
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  unsigned char *d = out;
+    const unsigned char *s = in;
+    while (n--) {
+        *d++ = *s++;
+    }
+    return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  const unsigned char *ptr1 = s1;
+    const unsigned char *ptr2 = s2;
+
+    while (n--) {
+        if (*ptr1 != *ptr2) {
+            return (int)(*ptr1 - *ptr2);  // 返回第一个不相等字节的差值
+        }
+        ptr1++;
+        ptr2++;
+    }
+    return 0;  // 如果前 n 个字节都相等，返回 0
 }
 
 #endif
