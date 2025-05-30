8c5173662bbed240e9200a402282e357a9ca8737
tracer-ysyx <tracer@ysyx.org>
2025-05-30 15:54:13 +0800

    > run NEMU ysyx_22040000 张三 Linux meowth 5.15.0-1026-intel-iotg #31-Ubuntu SMP Thu Feb 16 23:20:09 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:54:13 up 1 day,  2:12,  1 user,  load average: 0.89, 0.87, 0.89

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -2,14 +2,12 @@
 #include <nemu.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
+#define WIDTH_ADDR (VGACTL_ADDR + 2)
+#define HIGH_ADDR (VGACTL_ADDR)
+#define WIDTH 400
+#define HIGH  300
 
 void __am_gpu_init() {
-  int i;
-  int w = 0;  // TODO: get the correct width
-  int h = 0;  // TODO: get the correct height
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; i ++) fb[i] = i;
-  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -18,12 +16,23 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
     .width = 0, .height = 0,
     .vmemsz = 0
   };
+  cfg->width = inw(WIDTH_ADDR);
+  cfg->height = inw(HIGH_ADDR);
 }
-
+ //int x, y; void *pixels; int w, h; bool sync
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  int i,j;
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
+  uint32_t *p = (uint32_t *)ctl->pixels; 
+  uint32_t *fb = (uint32_t *)(uintptr_t)(FB_ADDR + (ctl->y*WIDTH + ctl->x) * sizeof(uint32_t));
+  for(i = 0;i < ctl->h;i++){
+    for(j = 0;j < ctl->w;j++)
+      fb[j] = *p++;
+    fb += WIDTH;
+  }
+    
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -72,8 +72,9 @@ static inline void update_screen() {
 #endif
 
 void vga_update_screen() {
-  // TODO: call `update_screen()` when the sync register is non-zero,
-  // then zero out the sync register
+  if(vgactl_port_base[1] != 0)
+    update_screen();
+  vgactl_port_base[1] = 0;
 }
 
 void init_vga() {
