54cc8ab4d0bfb6dc3f3be82835b4ba343f9e9c00
tracer-ysyx <tracer@ysyx.org>
2025-05-30 14:26:59 +0800

    > run NEMU ysyx_22040000 张三 Linux meowth 5.15.0-1026-intel-iotg #31-Ubuntu SMP Thu Feb 16 23:20:09 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:26:59 up 1 day, 45 min,  1 user,  load average: 1.25, 1.03, 0.65

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -63,6 +63,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
 }
 
 void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
+  printf("map_read addr: " FMT_PADDR ", len: %d, map: %s\n", addr, len, map ? map->name : "NULL");
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
