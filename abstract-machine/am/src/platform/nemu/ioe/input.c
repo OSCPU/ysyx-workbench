e52b525d1ec38010d52d8977b82fb51615ea717f
tracer-ysyx <tracer@ysyx.org>
2025-05-30 15:14:01 +0800

    > run NEMU ysyx_22040000 张三 Linux meowth 5.15.0-1026-intel-iotg #31-Ubuntu SMP Thu Feb 16 23:20:09 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:14:01 up 1 day,  1:32,  1 user,  load average: 2.78, 1.27, 0.91

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,7 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uint32_t key = inl(KBD_ADDR);  // 从键盘IO口读取按键信息
+  kbd->keydown = (key & 0x8000) != 0; // 高位为 1 表示按下，0 表示松开
+  kbd->keycode = key & 0x7fff;        // 低 15 位为按键码
 }
