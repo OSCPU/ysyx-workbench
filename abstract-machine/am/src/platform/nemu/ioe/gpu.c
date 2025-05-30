2df4fef32eb9566456c392c7b314b2d2e2df9d62
tracer-ysyx <tracer@ysyx.org>
2025-05-30 16:08:50 +0800

    > run NEMU ysyx_22040000 张三 Linux meowth 5.15.0-1026-intel-iotg #31-Ubuntu SMP Thu Feb 16 23:20:09 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:08:50 up 1 day,  2:26,  1 user,  load average: 1.41, 0.92, 0.91

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -21,17 +21,17 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
  //int x, y; void *pixels; int w, h; bool sync
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  // int i,j;
+  int i,j;
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
-  // uint32_t *p = (uint32_t *)ctl->pixels; 
-  // uint32_t *fb = (uint32_t *)(uintptr_t)(FB_ADDR + (ctl->y*WIDTH + ctl->x) * sizeof(uint32_t));
-  // for(i = 0;i < ctl->h;i++){
-  //   for(j = 0;j < ctl->w;j++)
-  //     fb[j] = *p++;
-  //   fb += WIDTH;
-  // }
+  uint32_t *p = (uint32_t *)ctl->pixels; 
+  uint32_t *fb = (uint32_t *)(uintptr_t)(FB_ADDR + (ctl->y*WIDTH + ctl->x) * sizeof(uint32_t));
+  for(i = 0;i < ctl->h;i++){
+    for(j = 0;j < ctl->w;j++)
+      fb[j] = *p++;
+    fb += WIDTH;
+  }
     
 }
 
