0c8b0308167b366d7c3d5008efee52ab0ebe6d86
tracer-ysyx <tracer@ysyx.org>
2025-06-05 16:52:23 +0800

    > run NEMU ysyx_22040000 张三 Linux meowth 5.15.0-1026-intel-iotg #31-Ubuntu SMP Thu Feb 16 23:20:09 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:52:23 up  2:12,  1 user,  load average: 0.71, 0.91, 0.90

---
+++ abstract-machine/am/src/riscv/npc/npc.h
@@ -0,0 +1,12 @@
+#include <riscv/riscv.h>
+
+# define DEVICE_BASE 0xa0000000
+#define MMIO_BASE 0xa0000000
+#define SERIAL_PORT     (DEVICE_BASE + 0x00003f8)
+#define KBD_ADDR        (DEVICE_BASE + 0x0000060)
+#define RTC_ADDR        (DEVICE_BASE + 0x0000048)
+#define VGACTL_ADDR     (DEVICE_BASE + 0x0000100)
+#define AUDIO_ADDR      (DEVICE_BASE + 0x0000200)
+#define DISK_ADDR       (DEVICE_BASE + 0x0000300)
+#define FB_ADDR         (MMIO_BASE   + 0x1000000)
+#define AUDIO_SBUF_ADDR (MMIO_BASE   + 0x1200000)
