2b60b792eed010f85ce86def5df6812a4b4bbb3d
tracer-ysyx <tracer@ysyx.org>
2025-06-05 16:30:33 +0800

    > run NEMU ysyx_22040000 张三 Linux meowth 5.15.0-1026-intel-iotg #31-Ubuntu SMP Thu Feb 16 23:20:09 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:30:33 up  1:50,  1 user,  load average: 1.39, 1.23, 0.94

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -1,6 +1,6 @@
 #include <am.h>
 #include <klib-macros.h>
-
+#include <npc.h>
 extern char _heap_start;
 int main(const char *args);
 
@@ -12,6 +12,7 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
 
 void putch(char ch) {
+  outb(SERIAL_PORT, ch);
 }
 
 void halt(int code) {
