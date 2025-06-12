51cc2c7b3d0472e1a48b79a703eeae7f481a8273
tracer-ysyx <tracer@ysyx.org>
2025-06-12 21:04:42 +0800

    > run NEMU ysyx_22040000 张三 Linux meowth 5.15.0-1026-intel-iotg #31-Ubuntu SMP Thu Feb 16 23:20:09 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:04:42 up  6:34,  1 user,  load average: 0.87, 0.65, 0.58

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -8,10 +8,10 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      // case 11:
-      //   ev.event = c->GPR1 == -1 ? EVENT_YIELD : EVENT_SYSCALL;
-      //   c->mepc += 4;
-      //   break;
+      case 11:
+        ev.event = c->GPR1 == -1 ? EVENT_YIELD : EVENT_SYSCALL;
+        c->mepc += 4;
+        break;
       default: ev.event = EVENT_ERROR; break;
     }
 
