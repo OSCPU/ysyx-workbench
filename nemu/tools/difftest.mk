a8cd06f1dc9238c707a46d08ee1585bc051b1f83
tracer-ysyx <tracer@ysyx.org>
2025-06-20 17:40:46 +0800

    > run NEMU ysyx_22040000 张三 Linux meowth 5.15.0-1026-intel-iotg #31-Ubuntu SMP Thu Feb 16 23:20:09 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:40:46 up  8:42,  1 user,  load average: 1.14, 0.76, 0.68

--- nemu/tools/difftest.mk
+++ nemu/tools/difftest.mk
@@ -16,13 +16,13 @@
 ifdef CONFIG_DIFFTEST
 DIFF_REF_PATH = $(NEMU_HOME)/$(call remove_quote,$(CONFIG_DIFFTEST_REF_PATH))
 DIFF_REF_SO = $(DIFF_REF_PATH)/build/$(GUEST_ISA)-$(call remove_quote,$(CONFIG_DIFFTEST_REF_NAME))-so
+
 MKFLAGS = GUEST_ISA=$(GUEST_ISA) SHARE=1 ENGINE=interpreter
 ARGS_DIFF = --diff=$(DIFF_REF_SO)
 
-ifndef CONFIG_DIFFTEST_REF_NEMU
+ifndef CONFIG_DIFFTEST_REF_NEMU #当nemu为参考时用以生成so
 $(DIFF_REF_SO):
 	$(MAKE) -s -C $(DIFF_REF_PATH) $(MKFLAGS)
 endif
-
 .PHONY: $(DIFF_REF_SO)
 endif
