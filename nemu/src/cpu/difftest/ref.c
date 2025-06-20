f018499d7f8ad967fe9792c005cb2b324df68653
tracer-ysyx <tracer@ysyx.org>
2025-06-20 15:59:16 +0800

    > run NEMU ysyx_22040000 张三 Linux meowth 5.15.0-1026-intel-iotg #31-Ubuntu SMP Thu Feb 16 23:20:09 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:59:16 up  7:01,  1 user,  load average: 0.87, 0.74, 0.64

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -18,16 +18,45 @@
 #include <difftest-def.h>
 #include <memory/paddr.h>
 
+#define NR_GPR MUXDEF(CONFIG_RVE, 16, 32)
+struct diff_context_t {
+  word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)];
+  word_t pc;
+  word_t csr[4];
+};
+
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  assert(0);
+  void *nemu_buf = (void *)guest_to_host(addr);
+  if(direction == DIFFTEST_TO_REF)  //dut -> ref (buf -> addr(nemu_buf))
+    memcpy(nemu_buf , buf , n);
+  else                              //ref -> dut (addr(nemu_buf) -> buf)
+    memcpy(buf , nemu_buf, n);
 }
 
+
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
-  assert(0);
+  int i = 0;
+  struct diff_context_t *dut_state = (struct diff_context_t *)dut;
+  if(direction == DIFFTEST_TO_REF){
+    for(i = 0;i < NR_GPR ; i++){
+      cpu.gpr[i] = dut_state->gpr[i];
+    }
+    cpu.pc = dut_state->pc;
+  }
+  else{
+    for(i = 0;i < NR_GPR ; i++){
+      dut_state->gpr[i] = cpu.gpr[i];
+    }
+    dut_state->pc = cpu.pc;
+    // dut_state->csr[0] = cpu.csr[0x300]; //mstatus
+    // dut_state->csr[1] = cpu.csr[0x305]; //mtvec
+    // dut_state->csr[2] = cpu.csr[0x341]; //mepc
+    // dut_state->csr[3] = cpu.csr[0x342]; //mcause
+  }
 }
 
 __EXPORT void difftest_exec(uint64_t n) {
-  assert(0);
+  cpu_exec(n);
 }
 
 __EXPORT void difftest_raise_intr(word_t NO) {
