c9486c6bd074421b2115bb160f4ea35004a664ff
tracer-ysyx <tracer@ysyx.org>
2025-06-20 17:56:40 +0800

    > run NEMU ysyx_22040000 张三 Linux meowth 5.15.0-1026-intel-iotg #31-Ubuntu SMP Thu Feb 16 23:20:09 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:56:40 up  8:58,  1 user,  load average: 0.50, 0.64, 0.66

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -36,6 +36,7 @@ run-env: $(BINARY) $(DIFF_REF_SO)
 run: run-env
 	$(call git_commit, "run NEMU")
 	$(NEMU_EXEC)
+	$(info [Makefile] NEMU_EXEC = $(NEMU_EXEC)) 
 
 gdb: run-env
 	$(call git_commit, "gdb NEMU")
